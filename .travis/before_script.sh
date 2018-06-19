cbd31606bb6978c10cc3ccaabfc2022883e7cbaf
Daniel Dragic <danac@guaranteed.site>
2018-06-19 17:48:43 +0200

    LICENSE.txt & license clause to ALL files

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-# Copyright © Magento, Inc. All rights reserved.
-# See COPYING.txt for license details.
+#/**
+#* Bolt magento2 plugin
+#*
+#* NOTICE OF LICENSE
+#*
+#* This source file is subject to the Open Software License (OSL 3.0)
+#* that is bundled with this package in the file LICENSE.txt.
+#* It is also available through the world-wide-web at this URL:
+#* http://opensource.org/licenses/osl-3.0.php
+#*
+#* @category   Bolt
+#* @package    Bolt_Boltpay
+#* @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
+#* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+#*/
 
 set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
@@ -26,5 +39,3 @@ php bin/magento setup:install -q \
 
 echo "Enabling production mode"
 php bin/magento deploy:mode:set production
-
-
