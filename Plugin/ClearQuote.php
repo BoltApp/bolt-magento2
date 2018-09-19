dd2c364e1f5f9deac3cd7b5ff49e999c1b7ee665
YevhenShBolt <yevhen@bolt.om>
2018-09-19 22:29:23 +0300

    Added customer.data and quote clear fix.

--- Plugin/ClearQuote.php
+++ Plugin/ClearQuote.php
@@ -1,13 +1,40 @@
 <?php
-
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 namespace Bolt\Boltpay\Plugin;
 
 use Magento\Checkout\Model\Session as CheckoutSession;
 
+/**
+ * Class ClearQuote
+ *
+ * @package Bolt\Boltpay\Plugin
+ */
 class ClearQuote
 {
+    /**
+     * @param CheckoutSession $subject
+     * @return CheckoutSession
+     * @throws \Exception
+     */
     public function afterClearQuote(CheckoutSession $subject)
     {
         $subject->setLoadInactive(false);
+        $subject->replaceQuote($subject->getQuote()->save());
+
+        return $subject;
     }
 }
