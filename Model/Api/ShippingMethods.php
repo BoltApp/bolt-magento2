4fd0dc3e310c02b0a3c292bc1e0d916dd4de3254
YevhenShBolt <yevhen@bolt.com>
2019-01-10 14:52:44 +0200

    cherry-pick skip validation on empty email

--- Model/Api/ShippingMethods.php
+++ Model/Api/ShippingMethods.php
@@ -311,7 +311,7 @@ class ShippingMethods implements ShippingMethodsInterface
     public function getShippingMethods($cart, $shipping_address)
     {
         try {
-            // $this->logHelper->addInfoLog($this->request->getContent());
+//            $this->logHelper->addInfoLog($this->request->getContent());
 
             $this->hookHelper->setCommonMetaData();
             $this->hookHelper->setHeaders();
@@ -337,7 +337,10 @@ class ShippingMethods implements ShippingMethodsInterface
             $this->sessionHelper->loadSession($quote);
 
             $addressData = $this->cartHelper->handleSpecialAddressCases($shipping_address);
-            $this->validateAddressData($addressData);
+
+            if (isset($addressData['email']) && $addressData['email'] !== null) {
+                $this->validateAddressData($addressData);
+            }
 
             $shippingOptionsModel = $this->shippingEstimation($quote, $addressData);
 
