434409315e60cef244b339b902e7820ee82795e7
YevhenShBolt <yevhen@bolt.com>
2019-02-27 12:25:46 +0200

    OrderCreate endpoint: refactoring and improvements.

--- Api/CreateOrderInterface.php
+++ Api/CreateOrderInterface.php
@@ -24,37 +24,21 @@ namespace Bolt\Boltpay\Api;
 interface CreateOrderInterface
 {
     /**
-     * Manage order.
+     * Create order.
      * Hook formats:
-     * {"quote_id":"350","reference":"Q3QR-R2JW-D9BC","transaction_id":"TAmdZdMTSkyYG","notification_type":"auth"}
-     * {"id":"TAgRDWjyT88sJ","reference":"D6JZ-TQ63-RFHJ","order":"353","type":"payment","amount":21485,
-     * "currency":"USD","status":"completed","display_id":"000000318"}
+     * [{"type":"order.create","order":{},"currency":"USD"}]
      *
      * @api
      *
-     * @param mixed $id
-     * @param mixed $reference
-     * @param mixed $order
      * @param mixed $type
-     * @param mixed $amount
+     * @param mixed $order
      * @param mixed $currency
-     * @param mixed $status
-     * @param mixed $display_id
-     * @param mixed $source_transaction_id
-     * @param mixed $source_transaction_reference
      *
      * @return void
      */
     public function execute(
-        $id = null,
-        $reference = null,
-        $order = null,
         $type = null,
-        $amount = null,
-        $currency = null,
-        $status = null,
-        $display_id = null,
-        $source_transaction_id = null,
-        $source_transaction_reference = null
+        $order = null,
+        $currency = null
     );
 }
