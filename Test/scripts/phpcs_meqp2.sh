797fc2c336d0858e39fd3d60a5a951adf3411326
Roopak Venkatakrishnan <roopak.v@gmail.com>
2019-06-24 09:13:03 -0700

    Php CS: show actual magento 2 errors (#312)
    * phpcs
    
    *  dash y
    
    * dash n

--- Test/scripts/phpcs_meqp2.sh
+++ Test/scripts/phpcs_meqp2.sh
@@ -10,8 +10,9 @@ composer show -i
 echo "{\"http-basic\":{\"repo.magento.com\":{\"username\":\"${MAGENTO_PUBLIC_KEY}\",\"password\":\"${MAGENTO_PRIVATE_KEY}\"}}}" > $HOME/.composer/auth.json
 
 cd ..
-composer create-project --repository=https://repo.magento.com magento/marketplace-eqp magento-coding-standard
+composer create-project --repository=https://repo.magento.com magento/marketplace-eqp
+composer create-project magento/magento-coding-standard --stability=dev magento-coding-standard -n
 
 cd magento-coding-standard
 
-vendor/bin/phpcs ../project --standard=MEQP2 --colors --severity=10 --extensions=php,phtml
+vendor/bin/phpcs ../project --standard=Magento2 --colors --severity=10 --extensions=php,phtml
