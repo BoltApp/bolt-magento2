0a16e809058396780dde959676e073a9eea96717
ethanwayda22 <ewayda2222@gmail.com>
2019-07-09 14:57:11 -0700

    add https

--- Test/scripts/ci.sh
+++ Test/scripts/ci.sh
@@ -32,9 +32,9 @@ NGROK_URL=$(curl http://127.0.0.1:4040/api/tunnels | grep  -oE '"public_url":"ht
 
 # TODO use proper store URL
 php bin/magento config:set web/unsecure/base_url "${NGROK_URL}"
-php bin/magento config:set web/secure/base_url "${NGROK_URL}"
+php bin/magento config:set web/secure/base_url "https://ethan-m2.dev.bolt.me/"
 php bin/magento config:set web/unsecure/base_link_url "${NGROK_URL}"
-php bin/magento config:set web/secure/base_link_url "${NGROK_URL}"
+php bin/magento config:set web/secure/base_link_url "https://ethan-m2.dev.bolt.me/"
 
 php bin/magento setup:upgrade
 php bin/magento cache:flush
