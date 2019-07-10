3a6e9fb31063a87f3ea042aa8c8c6aaef0591cc2
ethanwayda22 <ewayda2222@gmail.com>
2019-07-10 15:53:44 -0700

    switched back to none

--- Test/scripts/ci.sh
+++ Test/scripts/ci.sh
@@ -49,7 +49,7 @@ sudo sh -c 'echo "<VirtualHost *:80>
 </VirtualHost>" > /etc/apache2/sites-enabled/000-default.conf'
 sudo sh -c 'echo "<Directory /home/circleci/magento/>
         Options Indexes FollowSymLinks
-        AllowOverride All
+        AllowOverride None
         Require all granted
 </Directory>" >> /etc/apache2/apache2.conf'
 
