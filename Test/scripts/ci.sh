cee9f11c88e3826b9799637272915f59ac9c4a4c
ethanwayda22 <ewayda2222@gmail.com>
2019-07-16 16:06:48 -0700

    stripped logic for faster build + updated ngrok url to stop clashes

--- Test/scripts/ci.sh
+++ Test/scripts/ci.sh
@@ -9,32 +9,32 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 Test/scripts/install_magento.sh
 
 # install bolt plugin
-cd ..
-mkdir -p magento/app/code/Bolt/Boltpay
-cp -r project/. magento/app/code/Bolt/Boltpay/
-cd magento
-php bin/magento module:enable Bolt_Boltpay
+# cd ..
+# mkdir -p magento/app/code/Bolt/Boltpay
+# cp -r project/. magento/app/code/Bolt/Boltpay/
+# cd magento
+# php bin/magento module:enable Bolt_Boltpay
 
-# set config
-php bin/magento config:set payment/boltpay/active 1
-php bin/magento config:set payment/boltpay/api_key $BOLT_STAGING_MERCHANT_API_KEY
-php bin/magento config:set payment/boltpay/signing_secret $BOLT_STAGING_MERCHANT_SIGNING_SECRET
-php bin/magento config:set payment/boltpay/publishable_key_checkout $BOLT_STAGING_MERCHANT_PUBLISHABLE_KEY
+# # set config
+# php bin/magento config:set payment/boltpay/active 1
+# php bin/magento config:set payment/boltpay/api_key $BOLT_STAGING_MERCHANT_API_KEY
+# php bin/magento config:set payment/boltpay/signing_secret $BOLT_STAGING_MERCHANT_SIGNING_SECRET
+# php bin/magento config:set payment/boltpay/publishable_key_checkout $BOLT_STAGING_MERCHANT_PUBLISHABLE_KEY
 
 # install and run ngrok
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok.zip
 ./ngrok authtoken $NGROK_TOKEN
-./ngrok http 80 -hostname=ethan-${CIRCLE_BUILD_NUM}.dev.bolt.me &
+./ngrok http 80 -hostname=${CIRCLE_BUILD_NUM}.integrations.dev.bolt.me &
 sleep 10
 curl http://127.0.0.1:4040/api/tunnels
 NGROK_URL=$(curl http://127.0.0.1:4040/api/tunnels | grep  -oE '"public_url":"http://([^"]+)' | cut -c15-)/
 
 # TODO use proper store URL
-php bin/magento config:set web/unsecure/base_url "${NGROK_URL}"
-php bin/magento config:set web/secure/base_url "https://ethan-m2.dev.bolt.me/"
-php bin/magento config:set web/unsecure/base_link_url "${NGROK_URL}"
-php bin/magento config:set web/secure/base_link_url "https://ethan-m2.dev.bolt.me/"
+# php bin/magento config:set web/unsecure/base_url "${NGROK_URL}"
+# php bin/magento config:set web/secure/base_url "https://ethan-m2.dev.bolt.me/"
+# php bin/magento config:set web/unsecure/base_link_url "${NGROK_URL}"
+# php bin/magento config:set web/secure/base_link_url "https://ethan-m2.dev.bolt.me/"
 
 php -dmemory_limit=5G bin/magento setup:upgrade
 
@@ -65,13 +65,13 @@ cd ..
 mkdir log
 sudo service apache2 restart
 echo "restarted apache2"
-wget ethan-${CIRCLE_BUILD_NUM}.dev.bolt.me
+wget ${CIRCLE_BUILD_NUM}.integrations.dev.bolt.me
 #sudo APACHE_PID_FILE=apache.pid APACHE_RUN_USER=circleci APACHE_RUN_GROUP=circleci APACHE_LOG_DIR=~/log APACHE_RUN_DIR=~/magento apache2 -k start
 
-curl $NGROK_URL
-curl $NGROK_URL -o ~/project/artifacts/magento-index.html
-git clone -b add_ci_config git@github.com:BoltApp/integration-tests.git
-cd integration-tests
-npm install
-TEST_ENV=ci WDIO_CONFIG=localChrome npm run test-spec bolt/integration-tests/checkout/specs/magento2/magento2QuickCheckout.spec.ts
+# curl $NGROK_URL
+# curl $NGROK_URL -o ~/project/artifacts/magento-index.html
+# git clone -b add_ci_config git@github.com:BoltApp/integration-tests.git
+# cd integration-tests
+# npm install
+# TEST_ENV=ci WDIO_CONFIG=localChrome npm run test-spec bolt/integration-tests/checkout/specs/magento2/magento2QuickCheckout.spec.ts
 
