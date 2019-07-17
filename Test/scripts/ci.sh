21c8bedcd72219636d26cdf50276af981ab66835
ethanwayda22 <ewayda2222@gmail.com>
2019-07-17 13:31:24 -0700

    updated apache script

--- Test/scripts/ci.sh
+++ Test/scripts/ci.sh
@@ -57,8 +57,8 @@ sudo sh -c 'echo "<VirtualHost *:80>
 </VirtualHost>" > /etc/apache2/sites-enabled/000-default.conf'
 sudo sh -c 'echo "<Directory /home/circleci/magento/>
         Options Indexes FollowSymLinks
-        DirectoryIndex index.php
-        AllowOverride None
+        DirectoryIndex index.html index.php
+        AllowOverride ALL
         Require all granted
 </Directory>" >> /etc/apache2/apache2.conf'
 
