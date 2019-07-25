87b16596a8956bd3a7c7909068a86e6671f7d21a
ethanwayda22 <ewayda2222@gmail.com>
2019-07-25 11:35:12 -0700

    added correct sql logic

--- Test/scripts/ci.sh
+++ Test/scripts/ci.sh
@@ -51,8 +51,8 @@ php -dmemory_limit=5G bin/magento setup:static-content:deploy -f
 
 php bin/magento cache:flush
 INC_NUM=$((100*${CIRCLE_BUILD_NUM}))
-mysql -uroot -h 127.0.0.1 -e "USE magento2 ALTER TABLE quote AUTO_INCREMENT=${INC_NUM};"
-mysql -uroot -h 127.0.0.1 -e "USE magento2 ALTER TABLE sequence_order_1 AUTO_INCREMENT=${INC_NUM};"
+mysql -uroot -h 127.0.0.1 -e "USE magento2; ALTER TABLE quote AUTO_INCREMENT=${INC_NUM};"
+
 
 # tweak apache config
 echo "update apache config"
@@ -91,5 +91,3 @@ mkdir screenshots
 TEST_ENV=ci SCREENSHOT_DIR=./screenshots WDIO_CONFIG=localChrome npm run test-spec bolt/integration-tests/checkout/specs/magento2/magento2QuickCheckout.spec.ts
 TEST_ENV=ci SCREENSHOT_DIR=./screenshots WDIO_CONFIG=localChrome npm run test-spec bolt/integration-tests/checkout/specs/magento2/magento2discount.spec.ts
 TEST_ENV=ci WDIO_CONFIG=localChrome npm run test-spec bolt/integration-tests/checkout/specs/magento2/magento2LoggedInQuickCheckout.spec.ts
-
-
