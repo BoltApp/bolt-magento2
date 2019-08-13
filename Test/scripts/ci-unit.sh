1d75addcfa98117a29936055f9d8dda5e0785b46
Roopak Venkatakrishnan <roopak.v@gmail.com>
2019-08-13 09:09:28 -0700

    keep copy

--- Test/scripts/ci-unit.sh
+++ Test/scripts/ci-unit.sh
@@ -10,7 +10,13 @@ Test/scripts/install_magento.sh
 
 cd ..
 mkdir -p magento/app/code/Bolt/Boltpay
-cp project/* magento/app/code/Bolt/Boltpay/
+# magento requires the code to be in the magento installation dir
+# However if we copy codecov gets confused because of multiple sources.
+# So a quick fix is to keep a copy of the composer.json
+# TODO(roopakv): Initialize circle with the repo in the right place
+mv project/* magento/app/code/Bolt/Boltpay/
+mkdir -p project
+cp magento/app/code/Bolt/Boltpay/composer.json project/composer.json
 cp magento/app/code/Bolt/Boltpay/Test/Unit/phpunit.xml magento/dev/tests/unit/bolt_phpunit.xml
 echo "Starting Bolt Unit Tests"
 php magento/vendor/phpunit/phpunit/phpunit --verbose -c magento/dev/tests/unit/bolt_phpunit.xml --coverage-clover=./artifacts/coverage.xml
