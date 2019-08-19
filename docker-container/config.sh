439bad73cfbc1a599ca3042259b9ccf431eb30cf
Ethan Wayda <47336372+ethanwayda22@users.noreply.github.com>
2019-08-19 15:44:29 -0700

    remove credentials

--- docker-container/config.sh
+++ docker-container/config.sh
@@ -1,9 +1,9 @@
 #VARIABLES
 m2Version="master"
 magentoVersion="2.3.0"
-boltApiKey="643114b6ea382088a1fe81cb7964c87b457d80bc9577b5e8fbe33004b29bdbdf"
-boltSigningSecret="7f0d05d28446895959d9a605af1cf3de0fc59759e41365568f1e290ca1bf0d07"
-boltPublishableKey="_XG56mgHFPE2.yrz9CGZsVPw_.981564c9a3d6d1ad0473feb801faf91b9bda87b207119012e53beb64edcd0cea"
-ngrokUrlHTTP="https://m2-test.integrations.dev.bolt.me/"
-ngrokUrlHTTPS="https://m2-test.integrations.dev.bolt.me/"
+boltApiKey=""
+boltSigningSecret=""
+boltPublishableKey=""
+ngrokUrlHTTP="https://m2.name.dev.bolt.me/"
+ngrokUrlHTTPS="https://m2.name.dev.bolt.me/"
 installSampleData=true
