d2c2d3628c0ca06495868cc3a890268037dfa3be
Roopak Venkatakrishnan <roopak.v@gmail.com>
2019-10-15 19:33:31 -0700

    [PFS][Part2] Add upsert method (#397)
    * [PFS][Part2] Add upsert method
    
    * kazuki comment
    
    * tiny nit

--- Model/FeatureSwitchRepository.php
+++ Model/FeatureSwitchRepository.php
@@ -26,6 +26,30 @@ class FeatureSwitchRepository implements \Bolt\Boltpay\Api\FeatureSwitchReposito
         return $switch;
     }
 
+    /**
+     * Either creates a new switch or updates an existing switch.
+     *
+     * @param string $name
+     * @param boolean $value
+     * @param boolean $defaultValue
+     * @param int $rolloutPercentage
+     * @return \Bolt\Boltpay\Api\Data\FeatureSwitchInterface
+     */
+    public function upsertByName($name, $value, $defaultValue, $rolloutPercentage) {
+        $switch = null;
+        try {
+            $switch = $this->getByName($name);
+        } catch(NoSuchEntityException $e) {
+            // If the switch is not found we create it.
+            $switch = $this->featureSwitchFactory->create();
+            $switch->setName($name);
+        }
+        $switch->setValue($value);
+        $switch->setDefaultValue($defaultValue);
+        $switch->setRolloutPercentage($rolloutPercentage);
+        return $this->save($switch);
+    }
+
     public function save(\Bolt\Boltpay\Api\Data\FeatureSwitchInterface $switch) {
         $switch->getResource()->save($switch);
         return $switch;
