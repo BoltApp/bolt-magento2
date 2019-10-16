2ab60ad14367a84a015c5bcd90ddf6f94596c3d8
chaseadams509 <chase@bolt.com>
2019-10-16 13:14:51 -0700

    variablize transaction type

--- Helper/Order.php
+++ Helper/Order.php
@@ -1045,7 +1045,7 @@ class Order extends AbstractHelper
      */
     public function getTransactionState($transaction, $payment, $hookType = null)
     {
-        // $transactionType = $transaction->type;
+        $transactionType = $transaction->type;
         // // If it is an apm type, it needs to behave as regular payment/credit.
         // // Since there are previous states saved, it needs to mimic "cc_payment"/"cc_credit"
         // if (in_arry($transactionType, ["paypal_payment", "apm_payment"])) {
@@ -1054,8 +1054,8 @@ class Order extends AbstractHelper
         // if (in_arry($transactionType, ["paypal_refund", "apm_refund"])) {
         //     $transactionType = "cc_credit";
         // }
-        // $transactionState = $transactionType.":".$transaction->status;
-        $transactionState = $transaction->type.":".$transaction->status;
+        $transactionState = $transactionType.":".$transaction->status;
+        // $transactionState = $transaction->type.":".$transaction->status;
         $prevTransactionState = $payment->getAdditionalInformation('transaction_state');
         $transactionReference = $payment->getAdditionalInformation('transaction_reference');
         $transactionId = $payment->getAdditionalInformation('real_transaction_id');
