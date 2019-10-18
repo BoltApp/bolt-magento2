8af59406f434fbb3c9ffa7ff137b08b46f9b9df4
Roopak Venkatakrishnan <roopak.v@gmail.com>
2019-10-18 10:39:20 -0700

    comments

--- Helper/FeatureSwitch/Decider.php
+++ Helper/FeatureSwitch/Decider.php
@@ -40,7 +40,7 @@ class Decider extends AbstractHelper {
     private $_session;
 
     /**
-     * @var fsRepo
+     * @var FeatureSwitchRepository
      */
     private $_fsRepo;
 
@@ -76,13 +76,13 @@ class Decider extends AbstractHelper {
 
     /**
      * This method returns if a feature switch is enabled for a user.
-     * The way this is conputed is as follows:
+     * The way this is computed is as follows:
      * - Get feature switch id
      * - Set if unset.
      * - Add switch name as salt to ID and find md5 hash
      * - Get first 6 digits of MD5 and divide by 0xffffff. Should be between 0 and 1.
      * - Multiply previous value by 100
-     *   and comprare with rolloutPercentage to decide if in bucket.
+     *   and compare with rolloutPercentage to decide if in bucket.
      *
      * @param string $switchName
      * @param int $rolloutPercentage
@@ -115,7 +115,7 @@ class Decider extends AbstractHelper {
     }
 
     /**
-     * The returns if the switch is enabled.
+     * This returns if the switch is enabled.
      *
      * @param string $switchName name of the switch
      *
@@ -137,8 +137,7 @@ class Decider extends AbstractHelper {
         }
 
         if (!$switch) {
-            error_log("none");
-            // Something is really wrong, But we never dont to fail
+            // Something is really wrong, But we dont want to fail
             // if this sort of weird case occurs.
             return false;
         }
@@ -159,6 +158,6 @@ class Decider extends AbstractHelper {
      ***************************************************/
 
     public function isSampleSwitchEnabled() {
-        return $this->_manager->isSwitchEnabled(Definitions::M2_SAMPLE_SWITCH_NAME);
+        return $this->isSwitchEnabled(Definitions::M2_SAMPLE_SWITCH_NAME);
     }
 }
