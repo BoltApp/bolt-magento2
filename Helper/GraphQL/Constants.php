4f519c5329228a01ec2b49d013d5fb3cf4a3196f
Roopak Venkatakrishnan <roopak.v@gmail.com>
2019-10-20 20:49:13 -0700

    [Logging] API to send logs to bolt

--- Helper/GraphQL/Constants.php
+++ Helper/GraphQL/Constants.php
@@ -25,6 +25,20 @@ query GetFeatureSwitches($type: PluginType!, $version: String!) {
 }
 GQL;
 
+    /**
+     * The mutation to send logs from plugin to Bolt.
+     */
+    const SEND_LOGS_QUERY = <<<'GQL'
+mutation LogMerchantLogs($logs: [LogLine!]!) {
+  logMerchantLogs(logs: $logs){
+    isSuccessful
+  }
+}
+GQL;
+
+
     // Operation name for graphql query to retrieve feature switches.
     const GET_FEATURE_SWITCHES_OPERATION = 'GetFeatureSwitches';
+
+    const SEND_LOGS_OPERATION = 'LogMerchantLogs';
 }
