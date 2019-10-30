ff36774f170466b23e248dbc263012c0f20d2c89
Nathaniel Faries <njfaries@gmail.com>
2019-10-30 15:12:34 -0400

    Added SaveTest.php

---
+++ Test/Unit/Controller/Order/SaveTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Boltpay\Test\Unit\Controller\Order;
+
+use Bolt\Boltpay\Controller\Order\Save;
+use PHPUnit\Framework\TestCase;
+
+class SaveTest extends TestCase
+{
+
+    public function testExecute()
+    {
+
+    }
+}
