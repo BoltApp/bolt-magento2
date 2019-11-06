9aca096a79101015fdd5f728f481d2fa8393c8cd
daisy1754 <d-part@hotmail.co.jp>
2019-11-05 23:36:24 -0800

    track

--- view/frontend/web/js/track-checkout.js
+++ view/frontend/web/js/track-checkout.js
@@ -1,5 +1,23 @@
 /* Track checkout funnel transition in magento's default checkout */
 
-var currentStep = 'onCheckoutStart';
-BoltTrack.recordEvent("onCheckoutStart");
+var currentStep = '';
 
+function trackFunnel(step) {
+    if (step === currentStep) {
+        // prevent duplicate tracking
+        return;
+    }
+    currentStep = step;
+    BoltTrack.recordEvent(step);
+}
+
+trackFunnel("onCheckoutStart");
+
+// TODO: track onShippingAddressComplete when user complete filling in addresses
+// TODO: track onPaymentSubmit when user clicks pay
+
+window.addEventListener("hashchange", function() {
+   if (location.hash === "#payment") {
+       trackFunnel("onShippingOptionsComplete");
+   }
+});
