b0a4b94d8645045a91e77dcadea2673b789e4555
kajan-bolt <54901047+kajan-bolt@users.noreply.github.com>
2019-11-20 13:02:27 -0500

    Load Test M2 Plugin  (#429)
    * Load Tests to simulate large number of shoppers
    
    * Add comments to config.js

---
+++ Test/Load/inputs/address.js
@@ -0,0 +1,17 @@
+export const address = {
+    "street_address1": "1245 San Diego Loop",
+    "street_address2": "",
+    "street_address3": "",
+    "street_address4": "",
+    "locality": "Boston",
+    "region": "Massachusetts",
+    "postal_code": "02116",
+    "company": "",
+    "country": "United States",
+    "country_code": "US",
+    "name": "dummy user",
+    "first_name": "dummy",
+    "last_name": "user",
+    "phone": "4161111111",
+    "email_address": "kajan@bolt.com"
+};
