426859f115b452fb725bf16c341b6acc9336af08
johnmccombs1 <johnmccombs1@gmail.com>
2019-11-27 22:33:58 +0700

    Create model, resource model and collection for the credit card entity (#437)

---
+++ Model/ResourceModel/CustomerCreditCard/Collection.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2019 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Model\ResourceModel\CustomerCreditCard;
+use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+/**
+ * Class Collection
+ * @package Bolt\Boltpay\Model\ResourceModel\CustomerCreditCard
+ */
+class Collection extends AbstractCollection
+{
+    protected function _construct()
+    {
+        $this->_init(
+            'Bolt\Boltpay\Model\CustomerCreditCard',
+            'Bolt\Boltpay\Model\ResourceModel\CustomerCreditCard'
+        );
+    }
+}
