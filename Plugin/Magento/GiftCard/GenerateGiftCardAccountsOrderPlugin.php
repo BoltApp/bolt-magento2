0d9dcc93a5f711946330d275289164875c8af0ec
Daniel Dragic <danac@guaranteed.site>
2019-12-06 16:33:09 +0100

    Prevent Gift Card generation for Bolt orders if order status is pending_payment or canceled

---
+++ Plugin/Magento/GiftCard/GenerateGiftCardAccountsOrderPlugin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2019 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Bolt\Boltpay\Plugin\Magento\GiftCard;
+
+use Bolt\Boltpay\Model\Payment as BoltPayment;
+use Magento\Sales\Model\Order;
+
+class GenerateGiftCardAccountsOrderPlugin
+{
+    public function aroundExecute(
+        \Magento\GiftCard\Observer\GenerateGiftCardAccountsOrder $subject,
+        callable $proceed,
+        \Magento\Framework\Event\Observer $observer
+    ) {
+        $event = $observer->getEvent();
+        /** @var Order $order */
+        $order =  $event->getOrder();
+        if ($order->getPayment()->getMethod() != BoltPayment::METHOD_CODE ||
+            !in_array($order->getStatus(), [Order::STATE_PENDING_PAYMENT, Order::STATE_CANCELED])) {
+            return $proceed($observer);
+        }
+    }
+}
