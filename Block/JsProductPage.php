a3c9f85a165375561c9a953400b94bdd8cc44e96
Vitaliy Reznikov <vitaliy@bolt.com>
2020-01-07 11:17:14 +0000

    Add comments

--- Block/JsProductPage.php
+++ Block/JsProductPage.php
@@ -62,6 +62,13 @@ class JsProductPage extends Js {
         return $this->_product;
     }
 
+    /**
+     * Get maximum available quantity of product
+     * It used to show a warning if user tries to buy more then product quantity
+     * @return int -1 if we no need to control the rest of product,
+     *              0 if product is out of stock
+     *              positive value otherwise
+     */
     public function getProductQty()
     {
         $stockItem = $this->getProduct()->getExtensionAttributes()->getStockItem();
