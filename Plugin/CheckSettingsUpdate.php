69f95396c34e550a2afba19fd36238baf1cbce8a
Vitaliy Reznikov <vitaliy@bolt.com>
2020-03-25 15:32:03 +0200

    Make GetFeatureSwitch call when merchant key is changed (#600)
    * Make GetFeatureSwitch call when merchant key is changed

---
+++ Plugin/CheckSettingsUpdate.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Plugin;
+
+use Magento\Config\Model\Config as MagentoConfig;
+use Bolt\Boltpay\Helper\Cart as CartHelper;
+use Bolt\Boltpay\Helper\Config as ConfigHelper;
+use Bolt\Boltpay\Helper\FeatureSwitch\Manager as FeatureSwitchManager;
+use Bolt\Boltpay\Helper\Bugsnag;
+
+/**
+ * Class ClearQuote
+ *
+ * @package Bolt\Boltpay\Plugin
+ */
+class CheckSettingsUpdate
+{
+    /**
+     * @var CartHelper
+     */
+    private $cartHelper;
+
+    /**
+     * @var ConfigHelper
+     */
+    private $configHelper;
+
+    /*
+     * @var FeatureSwitchManager
+     */
+    protected $fsManager;
+
+    /**
+     * @var Bugsnag
+     */
+    private $bugsnag;
+
+    /**
+     * $string
+     */
+    private $oldApiKey;
+
+    /**
+     * @param CartHelper $cartHelper
+     * @param ConfigHelper $configHelper
+     * @param FeatureSwitchManager $fsManager
+     * @param Bugsnag $bugsnag
+     */
+    public function __construct(
+        CartHelper $cartHelper,
+        ConfigHelper $configHelper,
+        FeatureSwitchManager $fsManager,
+        Bugsnag $bugsnag
+    )
+    {
+        $this->cartHelper = $cartHelper;
+        $this->configHelper = $configHelper;
+        $this->fsManager = $fsManager;
+        $this->bugsnag = $bugsnag;
+    }
+
+    /**
+     * @param CheckoutSession $subject
+     * @return null Return null because method Save have no arguments
+     */
+    public function beforeSave(MagentoConfig $subject)
+    {
+        $this->oldApiKey = $this->configHelper->getApiKey();
+        return null;
+    }
+
+    /**
+     * @param CheckoutSession $subject
+     * @return CheckoutSession
+     */
+    public function afterSave(MagentoConfig $subject, $result)
+    {
+        $apiKey = $this->configHelper->getApiKey();
+        if ($apiKey && $apiKey != $this->oldApiKey) {
+            try {
+                $this->fsManager->updateSwitchesFromBolt();
+            } catch (\Exception $e) {
+                $this->bugsnag->notifyException($e);
+            }
+        }
+        return $result;
+    }
+}
+
---
+++ Test/Unit/Plugin/CheckSettingsUpdateTest.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Test\Unit\Helper;
+
+use PHPUnit\Framework\TestCase;
+use Magento\Config\Model\Config as MagentoConfig;
+use Bolt\Boltpay\Helper\Cart as CartHelper;
+use Bolt\Boltpay\Helper\Config as ConfigHelper;
+use Bolt\Boltpay\Helper\FeatureSwitch\Manager as FeatureSwitchManager;
+use Bolt\Boltpay\Helper\Bugsnag;
+use Bolt\Boltpay\Plugin\CheckSettingsUpdate;
+
+/**
+ * @coversDefaultClass \Bolt\Boltpay\Plugin\CheckSettingsUpdate
+ */
+class CheckSettingsUpdateTest extends TestCase
+{
+    const OLD_KEY = 'old_key';
+    const NEW_KEY = 'new_key';
+
+    /**
+     * @var CheckSettingsUpdate
+     */
+    private $currentMock;
+
+    /**
+     * @var MagentoConfig
+     */
+
+    private $magentoConfig;
+
+    /**
+     * @var CartHelper
+     */
+    private $cartHelper;
+
+    /**
+     * @var ConfigHelper
+     */
+    private $configHelper;
+
+    /*
+     * @var Manager
+     */
+    protected $fsManager;
+
+    /**
+     * @var Bugsnag
+     */
+    private $bugsnag;
+
+    public function setUp()
+    {
+        /*
+        $this->context = $this->getMockBuilder(Context::class)
+            ->disableOriginalConstructor()
+            ->getMock();*/
+
+        $this->cartHelper = $this->createMock(CartHelper::class);
+        $this->configHelper = $this->createMock(ConfigHelper::class);
+        $this->fsManager = $this->createMock(FeatureSwitchManager::class);
+        $this->bugsnag = $this->createMock(Bugsnag::class);
+        $this->magentoConfig = $this->createMock(magentoConfig::class);
+
+        $this->initCurrentMock([]);
+    }
+
+    /**
+     * @param array $methods
+     * @param bool $enableOriginalConstructor
+     * @param bool $enableProxyingToOriginalMethods
+     */
+    private function initCurrentMock($methods = [])
+    {
+        $builder = $this->getMockBuilder(CheckSettingsUpdate::class)
+            ->enableOriginalConstructor()
+            ->setConstructorArgs(
+                [
+                    $this->cartHelper,
+                    $this->configHelper,
+                    $this->fsManager,
+                    $this->bugsnag,
+                ]
+            )
+            ->setMethods($methods)
+            ->enableProxyingToOriginalMethods();
+
+        $this->currentMock = $builder->getMock();
+    }
+
+    private function callBeforeSaveAndAfterSave()
+    {
+        $beforeSaveResult = $this->currentMock->beforeSave($this->magentoConfig);
+        $afterSaveResult = $this->currentMock->afterSave($this->magentoConfig, $this->magentoConfig);
+        $this->assertNull($beforeSaveResult);
+        $this->assertSame($this->magentoConfig, $afterSaveResult);
+    }
+
+    /**
+     * @test
+     */
+    public function callBeforeSaveAndAfterSave_whenKeyNotChanged_doNothing()
+    {
+        $this->configHelper->expects($this->exactly(2))
+            ->method('getApiKey')->willReturn(self::OLD_KEY);
+        $this->fsManager->expects($this->never())
+            ->method('updateSwitchesFromBolt');
+        $this->callBeforeSaveAndAfterSave();
+    }
+
+    /**
+     * @test
+     */
+    public function callBeforeSaveAndAfterSave_whenKeyChanged_updateSwitches()
+    {
+        $this->configHelper->expects($this->at(0))
+            ->method('getApiKey')->willReturn(self::OLD_KEY);
+        $this->configHelper->expects($this->at(1))
+            ->method('getApiKey')->willReturn(self::NEW_KEY);
+        $this->fsManager->expects($this->once())
+            ->method('updateSwitchesFromBolt');
+        $this->callBeforeSaveAndAfterSave();
+    }
+
+    /**
+     * @test
+     */
+    public function callBeforeSaveAndAfterSave_whenKeyChangedToEmpty_doNothing()
+    {
+        $this->configHelper->expects($this->at(0))
+            ->method('getApiKey')->willReturn(self::OLD_KEY);
+        $this->configHelper->expects($this->at(1))
+            ->method('getApiKey')->willReturn('');
+        $this->fsManager->expects($this->never())
+            ->method('updateSwitchesFromBolt');
+        $this->callBeforeSaveAndAfterSave();
+    }
+
+    /**
+     * @test
+     */
+    public function callBeforeSaveAndAfterSave_whenKeyChangedAnfUpdateSwitchesThrowException_callBugsnag()
+    {
+        $this->configHelper->expects($this->at(0))
+            ->method('getApiKey')->willReturn(self::OLD_KEY);
+        $this->configHelper->expects($this->at(1))
+            ->method('getApiKey')->willReturn(self::NEW_KEY);
+        $e = new \Exception('test exception');
+        $this->fsManager->expects($this->once())
+            ->method('updateSwitchesFromBolt')->willThrowException($e);
+        $this->bugsnag->expects($this->once())
+            ->method('notifyException')->with($e);
+        $this->callBeforeSaveAndAfterSave();
+    }
+
+}
