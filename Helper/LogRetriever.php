b4dd449ca11f4febe397a84eff445190cfff4d5a
Nathaniel Faries <njfaries@gmail.com>
2020-03-25 11:50:35 -0400

    Added function to get tail of log file

--- Helper/LogRetriever.php
+++ Helper/LogRetriever.php
@@ -4,14 +4,67 @@ namespace Bolt\Boltpay\Helper;
 
 class LogRetriever
 {
+    const LOGPATH = "var/log/exception.log";
     public function _construct (
 
     ) {
 
     }
 
-    public function getExceptionLog()
+    public function getExceptionLog($logpath)
     {
-        return "Test string";
+        //open file
+        //get last 100 lines
+        return $this->customTail($logpath, 10, true);
+    }
+
+    private function customTail($logpath = self::LOGPATH, $lines = 100, $adaptive = true)
+    {
+        //Open file, return false if doesn't exist
+        $file = @fopen($logpath, "rb");
+        if ($file === false)
+        {
+            return false;
+        }
+
+        //set buffer according to lines we want
+        if (!$adaptive)
+        {
+            $buffer = 4096;
+        }
+        else
+        {
+            $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+        }
+
+        fseek($file, -1, SEEK_END);
+
+        //Correct for blank line at end of file
+        if (fread($file, 1) != "\n")
+        {
+            $lines -= 1;
+        }
+
+        $output = '';
+
+        while (ftell($file) > 0 && $lines >= 0)
+        {
+            $seek = min(ftell($file), $buffer);
+            fseek($file, -$seek, SEEK_CUR);
+            $chunk = fread($file, $seek);
+            $output = $chunk . $output;
+            fseek($file, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+            $lines -= substr_count($chunk, "\n");
+        }
+
+        //possible that with the buffer we read too many lines.
+        //find first newline char and remove all text before that
+        while ($lines++ < 0)
+        {
+            $output = substr($output, strpos($output, "\n") + 1);
+        }
+
+        fclose($file);
+        return trim($output);
     }
 }
