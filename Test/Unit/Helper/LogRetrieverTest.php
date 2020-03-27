3189f980d53a67d13b67c5190a29c46888f2751f
Nathaniel Faries <njfaries@gmail.com>
2020-03-27 18:56:35 -0400

    initial testing commit

--- Test/Unit/Helper/LogRetrieverTest.php
+++ Test/Unit/Helper/LogRetrieverTest.php
@@ -4,14 +4,39 @@ namespace Bolt\Boltpay\Test\Unit\Helper;
 
 use Bolt\Boltpay\Helper\LogRetriever;
 use PHPUnit\Framework\TestCase;
-use org\bovigo\vfs\vfsstream;
+use org\bovigo\vfs\vfsStream;
+
+/**
+ * Class LogRetrieverTest
+ * @package Bolt\Boltpay\Test\Unit\Helper
+ */
 
 class LogRetrieverTest extends TestCase
 {
+    /**
+     * @var string
+     */
+    private $virtualLogPath;
+
+    /**
+     * @var LogRetriever
+     */
+    private $logRetriever;
+
+    /**
+     * @var vfsStreamDirectory
+     */
+    private $root;
+
     protected function setUp()
     {
-        $fileReadResult = "Line 1 of log\nLine 2 of log\nLine 3 of log";
-        vfsStreamWrapper::register();
+        $structure = [
+            'log' => [
+                'exception.log' => "Line 1 of log\nLine 2 of log\nLine 3 of log"
+            ]
+        ];
+        $this->root = vfsStream::setup('root', null, $structure);
+        $this->logRetriever = $this->getMockBuilder(LogRetriever::class);
     }
 
     /**
@@ -20,5 +45,15 @@ class LogRetrieverTest extends TestCase
     public function getLog_success()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function getOneLineOfLog()
+    {
+        $expected = array(["Line 3 of log"]);
+
+        $this->assertEquals($expected, $this->logRetriever->getExceptionLog($this->virtualLogPath, 1));
     }
 }
