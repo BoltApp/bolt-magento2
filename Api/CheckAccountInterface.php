f0d176a29902026b420a516ddf8c121d6606e0c4
Vitaliy Reznikov <vitaliy@bolt.com>
2020-03-31 17:36:36 +0300

    [Order Management] ENP-24 Implement webhook to check if M2 user exists by email (#598)
    * Implement webhook to check if M2 user existst by email
    
    * Add class for output
    
    * Handle input parameters without Request class
    
    * Rename class
    
    * Add tests
    
    * Rename metrics
    
    * Change API method to GET
    
    * Rename field accountExist to accountExists
    
    * Fix tests

---
+++ Api/CheckAccountInterface.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Api;
+
+/**
+ * Check User Interface
+ * @api
+ */
+interface CheckAccountInterface
+{
+    /**
+     * @api
+     *
+     * @param mixed $email
+     * @return \Bolt\Boltpay\Model\Api\Data\AccountInfo
+     */
+    public function checkEmail($email = null);
+}
