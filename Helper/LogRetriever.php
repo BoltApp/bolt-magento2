65719607e5103e2f89f179d46adba384f2f31c36
Nathaniel Faries <njfaries@gmail.com>
2020-03-30 20:42:39 -0400

    used a much much simpler approach

--- Helper/LogRetriever.php
+++ Helper/LogRetriever.php
@@ -18,43 +18,6 @@ class LogRetriever
 
     private function customTail($logPath, $lines)
     {
-        //Open file, return informative error string if doesn't exist
-        $file = @fopen($logPath, "rb");
-        if ($file === false)
-        {
-            return "No file found at " . $logPath;
-        }
-
-        $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
-
-        fseek($file, -1, SEEK_END);
-
-        //Correct for blank line at end of file
-        if (fread($file, 1) != "\n")
-        {
-            $lines -= 1;
-        }
-
-        $output = '';
-
-        while (ftell($file) > 0 && $lines >= 0)
-        {
-            $seek = min(ftell($file), $buffer);
-            fseek($file, -$seek, SEEK_CUR);
-            $chunk = fread($file, $seek);
-            $output = $chunk . $output;
-            fseek($file, -mb_strlen($chunk, '8bit'), SEEK_CUR);
-            $lines -= substr_count($chunk, "\n");
-        }
-
-        //possible that with the buffer we read too many lines.
-        //find first newline char and remove all text before that
-        while ($lines++ < 0)
-        {
-            $output = substr($output, strpos($output, "\n") + 1);
-        }
-
-        fclose($file);
-        return trim($output);
+        return trim (`tail -n $lines $logPath`) ?: "No file found at " . $logPath;
     }
 }
