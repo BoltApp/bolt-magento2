5ae0ea16b78b9bf7090345d8764d5025d79b5ff9
Vitaliy Reznikov <vitaliy@bolt.com>
2020-04-01 11:46:16 +0000

    Enable feature switch by default

--- Helper/FeatureSwitch/Definitions.php
+++ Helper/FeatureSwitch/Definitions.php
@@ -101,7 +101,7 @@ class Definitions {
             self::NAME_KEY            => self::TRACKING_EVENT_OBSERVER_SWITCH,
             self::VAL_KEY             => true,
             self::DEFAULT_VAL_KEY     => false,
-            self::ROLLOUT_KEY         => 0
+            self::ROLLOUT_KEY         => 100
         )
     );
 }
