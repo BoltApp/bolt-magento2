799fd80823ef4b4968d8a7bdaceb5876dda47e39
johnmccombs1 <johnmccombs1@gmail.com>
2020-05-18 12:04:04 +0700

    Add custom url validation (#679)
    Co-authored-by: John McCombs <john.mccombs@bolt.com>

--- view/adminhtml/web/js/validation.js
+++ view/adminhtml/web/js/validation.js
@@ -2,10 +2,20 @@ require([
         'jquery',
         'mage/translate',
         'jquery/validate'],
-    function($){
+    function ($) {
         $.validator.addMethod(
             'validate-color', function (v) {
-                return v=="" || /^#[0-9A-F]{6}$/i.test(v);
-            }, $.mage.__('Please enter hex color code like "#00cccc"'));
+                return v == "" || /^#[0-9A-F]{6}$/i.test(v);
+            }, $.mage.__('Please enter hex color code like "#00cccc"')
+        );
+
+        $.validator.addMethod(
+            'validate-custom-url-api', function (v) {
+                var $length = v.length;
+                var last7Characters = v.substr($length - 7);
+                var last8Characters = v.substr($length - 8);
+                return (v == "" || last7Characters ==='bolt.me' || last8Characters === 'bolt.me/');
+            }, $.mage.__('Please enter custom url ends with "bolt.me"')
+        );
     }
 );
