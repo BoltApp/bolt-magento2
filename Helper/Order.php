730f352306c187278a53a18bb6aa510c95a597d5
Daniel Dragic <danac@guaranteed.site>
2020-05-25 09:53:41 +0200

    M2P-75 prevent additional credit memo creation attempt

--- Helper/Order.php
+++ Helper/Order.php
@@ -1816,10 +1816,9 @@ class Order extends AbstractHelper
                 $transactionType = Transaction::TYPE_REFUND;
                 $transactionId = $transaction->id . '-refund';
 
-                if ($this->featureSwitches->isCreatingCreditMemoFromWebHookEnabled()){
+                if (Hook::$fromBolt && $this->featureSwitches->isCreatingCreditMemoFromWebHookEnabled()){
                     $this->createCreditMemoForHookRequest($order, $transaction);
                 }
-
                 break;
 
             default:
