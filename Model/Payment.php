dc9c73a1e88205f3544384a8769434f2d2e6aaf9
John McCombs <john.mccombs@bolt.com>
2020-06-05 20:31:40 +0700

    Ignore 0.00 capture

--- Model/Payment.php
+++ Model/Payment.php
@@ -378,8 +378,17 @@ class Payment extends AbstractMethod
             $startTime = $this->metricsClient->getCurrentTime();
             $order = $payment->getOrder();
 
-            if ($amount <= 0) {
+            if ($amount < 0) {
                 throw new LocalizedException(__('Invalid amount for capture.'));
+            }
+
+            if ($amount < 0.01) {
+                ////////////////////////////////////////////////////////////////////////////////
+                // In certain circumstances, an amount's value of zero can be sent.
+                // This will then result in an exception by the Bolt API.
+                // In these instances, there is no need to call the Bolt API, so we simply return
+                ////////////////////////////////////////////////////////////////////////////////
+                return $this;
             }
 
             $realTransactionId = $payment->getAdditionalInformation('real_transaction_id');
