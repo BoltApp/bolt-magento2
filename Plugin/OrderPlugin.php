7643d1ed17263e46901016edd3631176cc7b0968
Daniel Dragic <danac@guaranteed.site>
2020-06-12 15:18:27 +0200

    postpone order place via OrderPlugin

--- Plugin/OrderPlugin.php
+++ Plugin/OrderPlugin.php
@@ -26,6 +26,11 @@ use Magento\Sales\Model\Order;
 class OrderPlugin
 {
     /**
+     * @var string|null
+     */
+    private $oldState;
+
+    /**
      * Override the default "new" order state with the "pending_payment"
      * unless the special "bolt_new" state is received in which case state "new" is used.
      *
@@ -35,6 +40,11 @@ class OrderPlugin
      */
     public function beforeSetState(Order $subject, $state)
     {
+        // Store the initial order state.
+        // Used in conditionally calling Order::place just once
+        // in the transition from Order::STATE_PENDING_PAYMENT to Order::STATE_NEW.
+        $this->oldState = $subject->getState();
+
         if (!$subject->getPayment() || $subject->getPayment()->getMethod() != \Bolt\Boltpay\Model\Payment::METHOD_CODE) {
             return [$state];
         }
@@ -72,5 +82,87 @@ class OrderPlugin
             $status = $subject->getConfig()->getStateDefaultStatus(Order::STATE_PENDING_PAYMENT);
         }
         return [$status];
+    }
+
+    /**
+     * Override Order place method.
+     * Skip execution when we just created the order.
+     * We call it manually later when bolt transaction is created.
+     *
+     * @param Order $subject
+     * @param callable $proceed
+     * @return Order
+     */
+    public function aroundPlace(Order $subject, callable $proceed)
+    {
+        // Move on if the order payment method is not Bolt
+        if (!$subject->getPayment() || $subject->getPayment()->getMethod() != \Bolt\Boltpay\Model\Payment::METHOD_CODE) {
+            return $proceed();
+        }
+        // Skip if the order did not reach Order::STATE_NEW state
+        if (!$subject->getState() || $subject->getState() == Order::STATE_PENDING_PAYMENT) {
+            return $subject;
+        }
+        return $proceed();
+    }
+
+    /**
+     * Prevent creating an order with an empty state / status.
+     * Since we rip Order::place out of the context by going around it
+     * first time it was called from QuoteManagement::submitQuote and
+     * call it manually later, the order would be created with no
+     * state / status set. They are ment to be set in the place call. As bypassing it
+     * with the around plugin does not prevent the after plugin execution we
+     * set the initial state here. We set the state to Order::STATE_NEW, as it
+     * was supposed to be set in the original method. Our beforeSetState plugin
+     * above then overrides it to Order::STATE_PENDING_PAYMENT if needed.
+     *
+     * @param Order $subject
+     * @param Order $result
+     * @return mixed
+     */
+    public function afterPlace($subject, $result)
+    {
+        // Skip if the order payment method is not Bolt
+        if (!$subject->getPayment() || $subject->getPayment()->getMethod() != \Bolt\Boltpay\Model\Payment::METHOD_CODE) {
+            return $result;
+        }
+        if (!$subject->getState()) {
+            $subject->setState(Order::STATE_NEW);
+            $subject->setStatus($subject->getConfig()->getStateDefaultStatus(Order::STATE_NEW));
+        }
+        return $result;
+    }
+
+    /**
+     * Manually call Order::place after the transition is made to Order::STATE_NEW state.
+     * Other parts of the Magento system expect this method to be called
+     * at the beginning of the order lifecycle. However, as we create the order before the
+     * authorization takes place, and may potentially cancel or delete it early on,
+     * we suppress the internal Order::place call and call it ourselves after
+     * the order is approved by Bolt.
+     *
+     * @param Order $subject
+     * @param Order $result
+     * @return array
+     */
+    public function afterSetState($subject, $result)
+    {
+        // Skip if the order payment method is not Bolt
+        if (!$subject->getPayment() || $subject->getPayment()->getMethod() != \Bolt\Boltpay\Model\Payment::METHOD_CODE) {
+            return $result;
+        }
+        // Place the order it it had just been moved to the Order::STATE_NEW state
+        if ($result->getState() == Order::STATE_NEW && $this->oldState == Order::STATE_PENDING_PAYMENT) {
+            $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/bolt.log');
+            $logger = new \Zend\Log\Logger();
+            $logger->addWriter($writer);
+            $logger->info("===============================================================");
+            $logger->info("=== OrderPlugin::afterSetState before Order::place ============");
+            $subject->place();
+            $logger->info("=== OrderPlugin::afterSetState after Order::place =============");
+            $logger->info("===============================================================");
+        }
+        return $result;
     }
 }
