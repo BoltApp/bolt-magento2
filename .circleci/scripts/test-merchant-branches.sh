c51d15dae8bfc97ca624088c22b24b6c276ac6f2
VincentGalloroBolt <64796353+VincentGalloroBolt@users.noreply.github.com>
2020-07-29 15:54:25 -0400

    try fix parameter formatting

--- .circleci/scripts/test-merchant-branches.sh
+++ .circleci/scripts/test-merchant-branches.sh
@@ -29,8 +29,9 @@ while IFS= read -r branchName || [[ -n "$branchName" ]]; do
     merchantBranch="ci/$branchName"
     echo "Beginning testing on branch: $merchantBranch"
 
-    echo '"parameters": { "run_rebase_and_unit_test": true, "rebase_and_unit_test_branch_name": "'"$CIRCLE_BRANCH"'" }'
-    /tmp/swissknife/trigger_pipeline.sh "$vcsType" "$user" "$repoName" "$merchantBranch" '"parameters": { "run_rebase_and_unit_test": true, "rebase_and_unit_test_branch_name": "'"$CIRCLE_BRANCH"'" }'
+    params='{ "parameters": { "run_rebase_and_unit_test": true, "rebase_and_unit_test_branch_name": "'"$CIRCLE_BRANCH"'" } }'
+    echo "params: $params"
+    /tmp/swissknife/trigger_pipeline.sh "$vcsType" "$user" "$repoName" "$merchantBranch" "$params"
 
   fi
 done < "$configFile"
