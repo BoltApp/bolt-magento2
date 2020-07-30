f49059d4b85b50a72575d992462638ae637dad32
VincentGalloroBolt <64796353+VincentGalloroBolt@users.noreply.github.com>
2020-07-30 10:10:03 -0400

    fix merchant rebasing

--- .circleci/scripts/rebase-and-unit-test.sh
+++ .circleci/scripts/rebase-and-unit-test.sh
@@ -2,7 +2,7 @@
 baseBranch=$1
 echo "Rebasing from branch: $baseBranch"
 
-if ! (git rebase $CIRCLE_BRANCH); then
+if ! (git rebase $baseBranch); then
   echo "Failed to rebase branch $CIRCLE_BRANCH"
   git rebase --abort
   exit 1
