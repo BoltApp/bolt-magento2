c49c818db6c9c1f8f14802d94de4db2aedb9955f
Leon McCottry <leon@guaranteed.network>
2020-07-31 19:29:58 +0200

    Update Test For Bolt\Boltpay\Setup\UpgradeSchema

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -20,9 +20,6 @@ use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
-/**
- * @codeCoverageIgnore
- */
 class UpgradeSchema implements UpgradeSchemaInterface
 {
     /**
