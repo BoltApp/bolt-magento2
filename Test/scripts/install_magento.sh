a6339c6700ca5e62ac6dfb255062aaf811a765ba
Vitaliy Reznikov <vitaliy@bolt.com>
2020-08-22 15:00:30 +0000

    Fix install script for magento 2.2

--- Test/scripts/install_magento.sh
+++ Test/scripts/install_magento.sh
@@ -31,7 +31,9 @@ while ! mysql -uroot -h 127.0.0.1 -e "SELECT 1" >/dev/null 2>&1; do
 done
 
 # TODO(vitaliy): Investigate the root of the issue. See https://github.com/magento/magento2/issues/24650
-sed -i 's/is_synchronous="false"//g' vendor/magento/module-inventory-catalog/etc/communication.xml
+if [ "${MAGENTO_VERSION}" == "2.3.0" ]; then
+    sed -i 's/is_synchronous="false"//g' vendor/magento/module-inventory-catalog/etc/communication.xml
+fi
 
 echo "Installing Magento..."
 mysql -uroot -h 127.0.0.1 -e 'CREATE DATABASE magento2;'
