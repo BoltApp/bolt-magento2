9538d5db44f73b93c9cd983be1b5b905812a9ccc
John McCombs <john.mccombs@bolt.com>
2020-08-28 22:24:24 +0700

    Added order pending payment status logging

---
+++ Plugin/OrderStatusLogging.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Bolt\Boltpay\Plugin;
+
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\ResourceModel\Order as ResourceOrder;
+
+class OrderStatusLogging
+{
+
+    /**
+     * @var \Bolt\Boltpay\Helper\Bugsnag
+     */
+    private $bugsnag;
+
+    public function __construct(
+        \Bolt\Boltpay\Helper\Bugsnag $bugsnag
+    ) {
+        $this->bugsnag = $bugsnag;
+    }
+
+    /**
+     * @param ResourceOrder                          $subject
+     * @param                                        $result
+     * @param \Magento\Framework\Model\AbstractModel $order
+     *
+     * @return mixed
+     */
+    public function afterSave(ResourceOrder $subject, $result, \Magento\Framework\Model\AbstractModel $order)
+    {
+        try {
+            if (!$order->getPayment() || $order->getPayment()->getMethod() != \Bolt\Boltpay\Model\Payment::METHOD_CODE){
+                return $result;
+            }
+
+            if ($order->getStatus() === Order::STATE_PENDING_PAYMENT || $order->getState() === Order::STATE_PENDING_PAYMENT){
+                $this->bugsnag->notifyError(
+                    "{$order->getIncrementId()} - Order status update",
+                    "\nStatus: {$order->getStatus()}, State: {$order->getState()}"
+                );
+            }
+
+
+
+        } catch (\Throwable $e) {
+            $this->bugsnag->notifyError(
+                "{$order->getIncrementId()} - Order status update - Error - {$e->getMessage()}",
+                "\nStatus: {$order->getStatus()}, State: {$order->getState()} \n Exception Trace: {$e->getTraceAsString()}"
+            );
+        }
+
+        return $result;
+    }
+}
