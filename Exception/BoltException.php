31e2be201283218c75505697e5a6d0293e26d52e
njfaries <nathaniel@bolt.com>
2020-12-11 17:56:24 +0000

    Adds quote object to BoltException

--- Exception/BoltException.php
+++ Exception/BoltException.php
@@ -19,9 +19,16 @@ namespace Bolt\Boltpay\Exception;
 
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Phrase;
+use Magento\Quote\Model\Quote;
 
 class BoltException extends LocalizedException
 {
+    /**
+     * @var Quote
+     */
+
+    protected $quote;
+
     /**
      * Override LocalizedException constructor because in older Magento versions
      * it does not take the $code parameter into account, defaulting it to 0.
@@ -29,10 +36,21 @@ class BoltException extends LocalizedException
      * @param \Magento\Framework\Phrase $phrase
      * @param \Exception $cause
      * @param int $code
+     * @param null|Quote $quote
      */
-    public function __construct(Phrase $phrase, \Exception $cause = null, $code = 0)
+ 
+    public function __construct(Phrase $phrase, \Exception $cause = null, $code = 0, $quote = null)
     {
         parent::__construct($phrase, $cause);
         $this->code = (int) $code;
+        $this->quote = $quote;
+    }
+
+    /**
+     * @return null|Quote
+     */
+    public function getQuote()
+    {
+        return $this->quote;
     }
 }
