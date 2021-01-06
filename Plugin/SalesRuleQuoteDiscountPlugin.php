ea1691bfe06fc0e6afc6a030ee3b1765fafd15c9
dumega <phpdev2012@126.com>
2021-01-05 23:42:56 -0800

    Reset BoltCollectSaleRuleDiscounts session data only if shippingAssignment has item

--- Plugin/SalesRuleQuoteDiscountPlugin.php
+++ Plugin/SalesRuleQuoteDiscountPlugin.php
@@ -31,11 +31,17 @@ class SalesRuleQuoteDiscountPlugin
     
     public function beforeCollect(\Magento\SalesRule\Model\Quote\Discount $subject, $quote, $shippingAssignment, $total)
     {
+        $items = $shippingAssignment->getItems();
+        
+        if (!count($items)) {
+            return [$quote, $shippingAssignment, $total];
+        }
+        
         $checkoutSession = $this->sessionHelper->getCheckoutSession();
         // Each time when collecting address discount amount, the BoltCollectSaleRuleDiscounts session data would be reset,
         // then it can store the updated info of applied sale rules.
         $checkoutSession->setBoltCollectSaleRuleDiscounts([]);
-       
+
         return [$quote, $shippingAssignment, $total];
     }
 }
