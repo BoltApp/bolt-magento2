c38d4e9c2f46c88820f1460c93b194748f06218f
Daniel Dragic <danac@guaranteed.site>
2021-02-01 14:47:06 +0100

    revert the change in DiscountCodeValidation that should befixed in another branch

--- Model/Api/DiscountCodeValidation.php
+++ Model/Api/DiscountCodeValidation.php
@@ -167,8 +167,8 @@ class DiscountCodeValidation extends UpdateCartCommon implements DiscountCodeVal
         }
 
         // Verify if the code is coupon or gift card and return proper object
-        $result = $this->verifyCouponCode($couponCode, $parentQuote);
-
+        $result = $this->verifyCouponCode($couponCode, $websiteId, $storeId);
+        
         list($coupon, $giftCard) = $result;
 
         $this->eventsForThirdPartyModules->dispatchEvent("beforeApplyDiscount", $parentQuote);
