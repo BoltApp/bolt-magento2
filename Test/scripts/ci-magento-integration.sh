519e5179f3e2aff7b0189f4a4370221abf4b172b
Aden <adentong@hotmail.com>
2021-02-25 22:18:19 -0500

    .

--- Test/scripts/ci-magento-integration.sh
+++ Test/scripts/ci-magento-integration.sh
@@ -34,4 +34,5 @@ sudo chmod -R 777 ../../..
 
 echo "Starting Bolt Integration Tests"
 ../../../vendor/bin/phpunit -d memory_limit=5G -c bolt_phpunit.xml --coverage-clover=/home/circleci/project/artifacts/coverage.xml
+cd ../../../..
 bash <(curl -s https://bolt-devops.s3-us-west-2.amazonaws.com/testing/codecov_uploader) -f /home/circleci/project/artifacts/coverage.xml -F $TEST_ENV
