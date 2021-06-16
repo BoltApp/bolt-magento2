e515a0e1c8b286a0a0438be0fef3beb88e66d71e
Ning W <phpdev2012@126.com>
2021-06-16 21:22:46 +0800

    Add tests

--- Test/Unit/Helper/CartTest.php
+++ Test/Unit/Helper/CartTest.php
@@ -91,6 +91,7 @@ use Zend\Serializer\Adapter\PhpSerialize as Serialize;
 use Bolt\Boltpay\Model\EventsForThirdPartyModules;
 use Magento\SalesRule\Model\RuleRepository;
 use Bolt\Boltpay\Helper\FeatureSwitch\Definitions;
+use Magento\CatalogInventory\Api\StockStateInterface as StockState;
 
 /**
  * @coversDefaultClass \Bolt\Boltpay\Helper\Cart
@@ -314,6 +315,9 @@ class CartTest extends BoltTestCase
 
     /** @var MockObject|ObjectManager */
     private $originalObjectManager;
+    
+    /** @var StockState */
+    private $stockState;
 
     /**
      * Setup test dependencies, called before each test
@@ -460,8 +464,8 @@ class CartTest extends BoltTestCase
             RuleRepository::class,
             ['getById']
         );
+        $this->stockState = $this->createMock(StockState::class);
         $this->currentMock = $this->getCurrentMock(null);
-
         $this->objectsToClean = [];
     }
 
@@ -514,7 +518,8 @@ class CartTest extends BoltTestCase
                     $this->deciderHelper,
                     $this->serialize,
                     $this->eventsForThirdPartyModules,
-                    $this->ruleRepository
+                    $this->ruleRepository,
+                    $this->stockState
                 ]
             )
             ->getMock();
@@ -719,7 +724,8 @@ class CartTest extends BoltTestCase
             $this->deciderHelper,
             $this->serialize,
             $this->eventsForThirdPartyModules,
-            $this->ruleRepository
+            $this->ruleRepository,
+            $this->stockState
         );
         static::assertAttributeEquals($this->checkoutSession, 'checkoutSession', $instance);
         static::assertAttributeEquals($this->productRepository, 'productRepository', $instance);
@@ -748,6 +754,7 @@ class CartTest extends BoltTestCase
         static::assertAttributeEquals($this->metricsClient, 'metricsClient', $instance);
         static::assertAttributeEquals($this->serialize, 'serialize', $instance);
         static::assertAttributeEquals($this->ruleRepository, 'ruleRepository', $instance);
+        static::assertAttributeEquals($this->stockState, 'stockState', $instance);
     }
 
     /**
@@ -785,6 +792,7 @@ class CartTest extends BoltTestCase
         static::assertAttributeInstanceOf(HookHelper::class, 'hookHelper', $instance);
         static::assertAttributeInstanceOf(CustomerRepository::class, 'customerRepository', $instance);
         static::assertAttributeInstanceOf(Registry::class, 'coreRegistry', $instance);
+        static::assertAttributeInstanceOf(StockState::class, 'stockState', $instance);
     }
 
     /**
@@ -6929,5 +6937,87 @@ ORDER
             [$this->immutableQuoteMock]
         );
         static::assertStringContainsString((string)$customerId, $result);
+    }
+    
+    /**
+     * @test
+     * that checkCartItemStockState throws BoltException with if a out-of-stock exception occurs
+     *
+     * @covers ::checkCartItemStockState
+     *
+     * @throws Exception from tested method
+     */
+    public function checkCartItemStockState_withExceptionIfCartItemOutOfStock_throwsBoltException()
+    {
+        $testItem = [
+            'reference'    => self::PRODUCT_ID,
+            'name'         => 'Test Product',
+            'total_amount' => 12345,
+            'unit_price'   => 12345,
+            'quantity'     => 1.0,
+            'sku'          => self::PRODUCT_SKU,
+            'type'         => 'physical',
+            'description'  => '',
+        ];
+        $getCartItemsResult = [[$testItem], 12345, 0];
+        $currentMock = $this->getCurrentMock(
+            [
+                'getCartItems',
+            ]
+        );
+        $currentMock->expects(static::once())->method('getCartItems')->willReturn($getCartItemsResult);
+        $this->stockState->method('verifyStock')->willReturn(false);
+        $this->expectException(BoltException::class);
+        $this->expectExceptionCode(2001005);
+        $this->expectExceptionMessage('The item [Test Product] is out of stock.');
+        TestHelper::invokeMethod(
+            $currentMock,
+            'checkCartItemStockState',
+            [$this->quoteMock, 2001005]
+        );
+    }
+    
+    /**
+     * @test
+     * that checkCartItemStockState throws BoltException if requested qty is not available
+     *
+     * @covers ::checkCartItemStockState
+     *
+     * @throws Exception from tested method
+     */
+    public function checkCartItemStockState_withExceptionIfCartItemNotEnoughQty_throwsBoltException()
+    {
+        $testItem = [
+            'reference'    => self::PRODUCT_ID,
+            'name'         => 'Test Product',
+            'total_amount' => 12345,
+            'unit_price'   => 12345,
+            'quantity'     => 1.0,
+            'sku'          => self::PRODUCT_SKU,
+            'type'         => 'physical',
+            'description'  => '',
+        ];
+        $getCartItemsResult = [[$testItem], 12345, 0];
+        $checkQtyResult = $this->getMockBuilder(DataObject::class)
+            ->setMethods(['getHasError'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $currentMock = $this->getCurrentMock(
+            [
+                'getCartItems',
+            ]
+        );
+        $currentMock->expects(static::once())->method('getCartItems')->willReturn($getCartItemsResult);
+        $this->stockState->method('verifyStock')->willReturn(true);
+        $checkQtyResult->expects(static::once())->method('getHasError')->willReturn(true);
+        $this->stockState->method('checkQuoteItemQty')->willReturn($checkQtyResult);
+        $this->expectException(BoltException::class);
+        $this->expectExceptionCode(2001005);
+        $this->expectExceptionMessage('The requested qty of [Test Product] is not available');
+        TestHelper::invokeMethod(
+            $currentMock,
+            'checkCartItemStockState',
+            [$this->quoteMock, 2001005]
+        );
     }
 }
