bbf9254b998cda2804ce91fdacb4e4a8516e0dfe
Ning W <phpdev2012@126.com>
2021-07-17 08:36:19 +0800

    M2P-640 Add support for Mageworx rewards (#1356)
    * Add support for Mageworx rewards
    
    * add tests
    
    * update per comment
    
    * Fix syntax error
    
    * Fix error if MageWorx Rewards plugin not installed
    
    * Add inputplaceholder

--- Model/EventsForThirdPartyModules.php
+++ Model/EventsForThirdPartyModules.php
@@ -48,6 +48,7 @@ use Bolt\Boltpay\ThirdPartyModules\Route\Route as Route_Route;
 use Bolt\Boltpay\Helper\Bugsnag;
 use Bolt\Boltpay\ThirdPartyModules\ImaginationMedia\TmwGiftCard as ImaginationMedia_TmwGiftCard;
 use Bolt\Boltpay\ThirdPartyModules\Amasty\Preorder as Amasty_Preorder;
+use Bolt\Boltpay\ThirdPartyModules\MageWorx\RewardPoints as MageWorx_RewardPoints;
 use Exception;
 
 class EventsForThirdPartyModules
@@ -549,6 +550,15 @@ class EventsForThirdPartyModules
                     ],
                     "boltClass" => ImaginationMedia_TmwGiftCard::class,
                 ],
+                "MageWorx_RewardPoints" => [
+                    "module" => "MageWorx_RewardPoints",
+                    "sendClasses" => [
+                        "MageWorx\RewardPoints\Api\CustomerBalanceRepositoryInterface",
+                        "MageWorx\RewardPoints\Model\PointCurrencyConverter",
+                        "MageWorx\RewardPoints\Helper\Data"
+                    ],
+                    "boltClass" => MageWorx_RewardPoints::class,
+                ],
             ],
         ],
         /** @see \Bolt\Boltpay\Model\Api\UpdateDiscountTrait::verifyCouponCode */
@@ -893,6 +903,11 @@ class EventsForThirdPartyModules
                     "sendClasses" => ["Aheadworks\RewardPoints\Api\CustomerRewardPointsManagementInterface"],
                     "boltClass"   => Aheadworks_RewardPoints::class,
                 ],
+                "MageWorx_RewardPoints" => [
+                    "module"      => "MageWorx_RewardPoints",
+                    "sendClasses" => ["MageWorx\RewardPoints\Helper\Data"],
+                    "boltClass"   => MageWorx_RewardPoints::class,
+                ],
             ]
         ],
         "saveSessionData" => [
@@ -1030,6 +1045,15 @@ class EventsForThirdPartyModules
                     'module' => 'Amasty_Preorder',
                     'sendClasses' => ['Amasty\Preorder\Helper\Data'],
                     'boltClass' => Amasty_Preorder::class,
+                ],
+            ],
+        ],
+        "getAdditionalQuoteTotalsConditions" => [
+            "listeners" => [
+                "MageWorx_RewardPoints" => [
+                    "module" => "MageWorx_RewardPoints",
+                    "sendClasses" => ["MageWorx\RewardPoints\Helper\Data"],
+                    "boltClass" => MageWorx_RewardPoints::class,
                 ],
             ],
         ],
---
+++ Plugin/MageWorx/RewardPoints/Controller/Checkout/ApplyPlugin.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2017-2021 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Plugin\MageWorx\RewardPoints\Controller\Checkout;
+
+use Magento\Customer\Model\Session as CustomerSession;
+use Bolt\Boltpay\ThirdPartyModules\MageWorx\RewardPoints as BoltMageWorxRewards;
+
+class ApplyPlugin
+{
+    /**
+     * @var CustomerSession
+     */
+    private $customerSession;
+
+
+    /**
+     * MageWorxRewardPointsControllerCheckoutApplyPlugin constructor.
+     *
+     * @param \Magento\Customer\Model\Session $customerSession
+     */
+    public function __construct(
+        CustomerSession $customerSession
+    ) {
+        $this->customerSession = $customerSession;
+    }
+
+    /**
+     * Check how the customer choose to spend the reward points.
+     *
+     * @return mixed
+     */
+    public function beforeExecute(\MageWorx\RewardPoints\Controller\Checkout\Apply $subject) {
+        if ($subject->getRequest()->isXmlHttpRequest()) {
+            $amount = $subject->getRequest()->getParam('amount');
+            if (is_null($amount)) {
+                $this->customerSession->setBoltMageWorxRewardsMode(
+                    BoltMageWorxRewards::MAGEWORX_REWARDS_APPLY_MODE_ALL
+                );
+            } else {
+                $this->customerSession->setBoltMageWorxRewardsMode(
+                    BoltMageWorxRewards::MAGEWORX_REWARDS_APPLY_MODE_PART
+                );
+            }
+        }
+
+        return null;
+    }
+}
