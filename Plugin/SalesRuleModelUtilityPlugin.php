0845dfbf99607875533247c4a4a11a8204867731
Ning W <phpdev2012@126.com>
2021-08-04 20:49:21 +0800

    Exclude "useless" cart price rule

--- Plugin/SalesRuleModelUtilityPlugin.php
+++ Plugin/SalesRuleModelUtilityPlugin.php
@@ -17,6 +17,7 @@
 namespace Bolt\Boltpay\Plugin;
 
 use Bolt\Boltpay\Helper\Session as SessionHelper;
+use Bolt\Boltpay\Helper\Discount as DiscountHelper;
 
 class SalesRuleModelUtilityPlugin
 {    
@@ -29,6 +30,26 @@ class SalesRuleModelUtilityPlugin
         $this->sessionHelper = $sessionHelper;
     }
     
+    /**
+     * Save the discount amount of item into checkout session, so we can get actual discount amount of coupon.
+     */
+    public function beforeMinFix(\Magento\SalesRule\Model\Utility $subject, $discountData, $item, $qty)
+    {
+        $checkoutSession = $this->sessionHelper->getCheckoutSession();
+        $savedRuleId = $checkoutSession->getBoltNeedCollectSaleRuleDiscounts('');      
+        if (!empty($savedRuleId)) {
+            $checkoutSession->setBoltDiscountBreakdown( [
+                'item_discount' => $item->getDiscountAmount(),
+                'rule_id' => $savedRuleId,
+            ]);
+        }
+        
+        return [$discountData, $item, $qty];
+    }
+    
+    /**
+     * Save actual discount amount of coupon into checkout session.
+     */
     public function afterMinFix(\Magento\SalesRule\Model\Utility $subject,
                                    $result, $discountData, $item, $qty)
     {
@@ -37,15 +58,22 @@ class SalesRuleModelUtilityPlugin
         if (!empty($savedRuleId)) {
             // If the sale rule has no coupon, its discount amount can not be retrieved directly,
             // so we store the discount amount in the checkout session with the rule id as key.
-            $boltCollectSaleRuleDiscounts = $checkoutSession->getBoltCollectSaleRuleDiscounts([]);            
-            if (!isset($boltCollectSaleRuleDiscounts[$savedRuleId])) {
-                $boltCollectSaleRuleDiscounts[$savedRuleId] = $discountData->getAmount();            
-            } else {
-                $boltCollectSaleRuleDiscounts[$savedRuleId] += $discountData->getAmount();
+            $boltDiscountBreakdown = $checkoutSession->getBoltDiscountBreakdown([]);
+            if (!empty($boltDiscountBreakdown) && $boltDiscountBreakdown['rule_id'] == $savedRuleId) {
+                $discountAmount = $discountData->getAmount() - $boltDiscountBreakdown['item_discount'];
+                if ($discountAmount >= DiscountHelper::MIN_NONZERO_VALUE) {
+                    $boltCollectSaleRuleDiscounts = $checkoutSession->getBoltCollectSaleRuleDiscounts([]);
+                    if (!isset($boltCollectSaleRuleDiscounts[$savedRuleId])) {
+                        $boltCollectSaleRuleDiscounts[$savedRuleId] = $discountAmount;            
+                    } else {
+                        $boltCollectSaleRuleDiscounts[$savedRuleId] += $discountAmount;
+                    }
+                    $checkoutSession->setBoltCollectSaleRuleDiscounts($boltCollectSaleRuleDiscounts);
+                }
             }
-            $checkoutSession->setBoltCollectSaleRuleDiscounts($boltCollectSaleRuleDiscounts);
-            $checkoutSession->setBoltNeedCollectSaleRuleDiscounts(''); 
         }
+        $checkoutSession->setBoltNeedCollectSaleRuleDiscounts('');
+        $checkoutSession->setBoltDiscountBreakdown([]);
 
         return $result;
     }
