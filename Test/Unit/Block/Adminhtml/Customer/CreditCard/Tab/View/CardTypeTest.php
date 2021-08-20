0fcbfd2e92d42a7e4e4fa96a19dbd08818614d20
John McCombs <john.mccombs@bolt.com>
2021-08-21 00:26:59 +0700

    Use integration test instead of unit test for all methods in class Bolt\Boltpay\Test\Unit\Block\Adminhtml\Customer\CreditCard\Tab\View\CardNumberTest\CardTypeTest

--- Test/Unit/Block/Adminhtml/Customer/CreditCard/Tab/View/CardTypeTest.php
+++ Test/Unit/Block/Adminhtml/Customer/CreditCard/Tab/View/CardTypeTest.php
@@ -19,20 +19,56 @@ namespace Bolt\Boltpay\Test\Unit\Block\Adminhtml\Customer\CreditCard\Tab\View;
 
 use Bolt\Boltpay\Block\Adminhtml\Customer\CreditCard\Tab\View\CardType;
 use Bolt\Boltpay\Test\Unit\BoltTestCase;
+use Bolt\Boltpay\Model\CustomerCreditCardFactory;
+use Bolt\Boltpay\Test\Unit\TestUtils;
+use Magento\Framework\App\ObjectManager;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Store\Api\WebsiteRepositoryInterface;
 
 class CardTypeTest extends BoltTestCase
 {
-    const CARD_TYPE = 'Visa';
+    const ID = '1110';
+    const CONSUMER_ID = '2222';
+    const CREDIT_CARD_ID = '4444';
+    const CARD_INFO = '{"last4":"4445","display_network":"Visa"}';
 
     /** @var CardType */
     private $block;
+    /** @var ObjectManager */
+    private $objectManager;
+    /** @var  */
+    private $customerCreditCardFactory;
 
     protected function setUpInternal()
     {
-        $this->block = $this->getMockBuilder(CardType::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['getCardType'])
-            ->getMock();
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->block = $this->objectManager->create(CardType::class);
+        $store = $this->objectManager->get(StoreManagerInterface::class);
+        $storeId = $store->getStore()->getId();
+
+        $websiteRepository = $this->objectManager->get(WebsiteRepositoryInterface::class);
+        $websiteId = $websiteRepository->get('base')->getId();
+        $customer = TestUtils::createCustomer($websiteId, $storeId, [
+            "street_address1" => "street",
+            "street_address2" => "",
+            "locality" => "Los Angeles",
+            "region" => "California",
+            'region_code' => 'CA',
+            'region_id' => '12',
+            "postal_code" => "11111",
+            "country_code" => "US",
+            "country" => "United States",
+            "name" => "lastname firstname",
+            "first_name" => "firstname",
+            "last_name" => "lastname",
+            "phone_number" => "11111111",
+            "email_address" => "johntest11@bolt.com",
+        ]);
+        $this->customerCreditCardFactory = $this->objectManager->create(CustomerCreditCardFactory::class)->create()
+            ->setCustomerId($customer->getId())->setConsumerId(self::CONSUMER_ID)
+            ->setCreditCardId(self::CREDIT_CARD_ID)->setCardInfo(self::CARD_INFO)
+            ->save();
     }
 
     /**
@@ -40,11 +76,8 @@ class CardTypeTest extends BoltTestCase
      */
     public function render()
     {
-        $this->block->expects(self::once())
-            ->method('getCardType')
-            ->willReturn(self::CARD_TYPE);
-        $result = $this->block->render($this->block);
-
-        $this->assertEquals(self::CARD_TYPE, $result);
+        $result = $this->block->render($this->customerCreditCardFactory);
+        $this->assertEquals('Visa', $result);
+        TestUtils::cleanupSharedFixtures([$this->customerCreditCardFactory]);
     }
 }
