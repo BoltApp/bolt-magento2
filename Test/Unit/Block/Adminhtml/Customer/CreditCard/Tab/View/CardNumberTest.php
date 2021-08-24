0e79f9b2bb17043cb1d1057878b14fdab75a904a
johnmccombs1 <john.mccombs@bolt.com>
2021-08-24 17:23:25 +0300

    Use integration test instead of unit test for all methods in class Bolt\Boltpay\Test\Unit\Block\Adminhtml\Customer\CreditCard\Tab\View\CardNumberTest\CardNumberTest (#1382)

--- Test/Unit/Block/Adminhtml/Customer/CreditCard/Tab/View/CardNumberTest.php
+++ Test/Unit/Block/Adminhtml/Customer/CreditCard/Tab/View/CardNumberTest.php
@@ -18,7 +18,12 @@
 namespace Bolt\Boltpay\Test\Unit\Block\Adminhtml\Customer\CreditCard\Tab\View;
 
 use Bolt\Boltpay\Block\Adminhtml\Customer\CreditCard\Tab\View\CardNumber;
+use Bolt\Boltpay\Model\CustomerCreditCardFactory;
 use Bolt\Boltpay\Test\Unit\BoltTestCase;
+use Bolt\Boltpay\Test\Unit\TestUtils;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Store\Api\WebsiteRepositoryInterface;
 
 /**
  * Class CardNumber
@@ -28,15 +33,45 @@ class CardNumberTest extends BoltTestCase
 {
     const LAST_4_DIGIT_CARD = 'XXXX-4444';
 
+    const ID = '1110';
+    const CONSUMER_ID = '11121';
+    const CREDIT_CARD_ID = '11131';
+    const CARD_INFO = '{"last4":"4444","display_network":"Visa"}';
+
     /** @var CardNumber */
     private $block;
+    private $objectManager;
+    private $customerCreditCardFactory;
 
     protected function setUpInternal()
     {
-        $this->block = $this->getMockBuilder(CardNumber::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['getCardLast4Digit'])
-            ->getMock();
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->block = $this->objectManager->create(CardNumber::class);
+        $store = $this->objectManager->get(StoreManagerInterface::class);
+        $storeId = $store->getStore()->getId();
+
+        $websiteRepository = $this->objectManager->get(WebsiteRepositoryInterface::class);
+        $websiteId = $websiteRepository->get('base')->getId();
+        $customer = TestUtils::createCustomer($websiteId, $storeId, [
+            "street_address1" => "street",
+            "street_address2" => "",
+            "locality" => "Los Angeles",
+            "region" => "California",
+            'region_code' => 'CA',
+            'region_id' => '12',
+            "postal_code" => "11111",
+            "country_code" => "US",
+            "country" => "United States",
+            "name" => "lastname firstname",
+            "first_name" => "firstname",
+            "last_name" => "lastname",
+            "phone_number" => "11111111",
+            "email_address" => "johntest11@bolt.com",
+        ]);
+        $this->customerCreditCardFactory = $this->objectManager->create(CustomerCreditCardFactory::class)->create()
+            ->setCustomerId($customer->getId())->setConsumerId(self::CONSUMER_ID)
+            ->setCreditCardId(self::CREDIT_CARD_ID)->setCardInfo(self::CARD_INFO)
+            ->save();
     }
 
     /**
@@ -44,11 +79,8 @@ class CardNumberTest extends BoltTestCase
      */
     public function render()
     {
-        $this->block->expects(self::once())
-            ->method('getCardLast4Digit')
-            ->willReturn(self::LAST_4_DIGIT_CARD);
-        $result = $this->block->render($this->block);
-
+        $result = $this->block->render($this->customerCreditCardFactory);
         $this->assertEquals(self::LAST_4_DIGIT_CARD, $result);
+        TestUtils::cleanupSharedFixtures([$this->customerCreditCardFactory]);
     }
 }
