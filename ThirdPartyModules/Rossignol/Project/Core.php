0c24a2441d7bab86b18ef5f3b5203c219d00c7ef
Ning W <phpdev2012@126.com>
2021-09-27 23:07:20 +0800

    Disable PPC if product is out of stock

--- ThirdPartyModules/Rossignol/Project/Core.php
+++ ThirdPartyModules/Rossignol/Project/Core.php
@@ -17,8 +17,6 @@
 
 namespace Bolt\Boltpay\ThirdPartyModules\Rossignol\Project;
 
-use Bolt\Boltpay\Helper\Config as ConfigHelper;
-use Bolt\Boltpay\Helper\Cart as CartHelper;
 use Bolt\Boltpay\Helper\Bugsnag as BugsnagHelper;
 use Magento\Eav\Api\AttributeSetRepositoryInterface;
 use Magento\Catalog\Model\ProductRepository;
@@ -34,42 +32,58 @@ class Core
      * @var BugsnagHelper
      */
     private $bugsnagHelper;
+    
+    /**
+     * @var AttributeSetRepositoryInterface
+     */
+    private $attributeSetRepository;
 
     /**
      * Core constructor.
      *
+     * @param AttributeSetRepositoryInterface $attributeSetRepository
      * @param BugsnagHelper $bugsnagHelper
      */
     public function __construct(
+        AttributeSetRepositoryInterface $attributeSetRepository,
         BugsnagHelper $bugsnagHelper
     ) {
+        $this->attributeSetRepository = $attributeSetRepository;
         $this->bugsnagHelper = $bugsnagHelper;
     }
     
     /**
      * @param bool $result
-     * @param \Project\Core\Helper\Sales $restrictSalesHelper
+     * @param \Project\Core\Helper\Sales $projectSalesHelper
      * @param \Magento\Framework\View\Element\Template $block
      * @param \Magento\Quote\Model\Quote $quote
      * @return bool
      */
     public function filterShouldDisableBoltCheckout(
         $result,
-        $restrictSalesHelper,
+        $projectSalesHelper,
         $block,
         $quote
     ) {
         try {
             $disableTemplateList = ['Bolt_Boltpay::button_product_page.phtml'];
+            $skiItemGroups = 'A1,B1,F1,H1,H2,A9,B9,F9';
+            $apparelItemGroups = 'A2,A3,A5,B2,B3,F2,F3,H3,H4,H5,H6,H7,H8,H9,HA,N2,PS,S1,S8,TA,TB,TC,TD,TE,TF,TG,TH,TI,TJ,TK,TL,TM,TN,TO,TP,TQ,TR,TS,TT,TU,TW,TX,TY,TZ,L1,N5,R1,S7,TV';
             if (!$result
                 && 'catalog_product_view' === $block->getRequest()->getFullActionName()
                 && in_array($block->getTemplate(), $disableTemplateList)) {
                 $product = $block->getProduct();
-                $canSell = $restrictSalesHelper->canSell($product);
-                $isKit   = $restrictSalesHelper->isProductWithKit($product);
-                if ($isKit) {
-                    $canSell = $restrictSalesHelper->isEcommFeaturesEnabled();
+                $attributeSet = $this->attributeSetRepository->get($product->getAttributeSetId());
+                $attributeSetName = $attributeSet->getAttributeSetName();
+                if (in_array($attributeSetName, explode(',', $skiItemGroups))) {
+                    $isKit = $projectSalesHelper->isProductWithKit($product);
+                    $canSell = $projectSalesHelper->isEcommFeaturesEnabled() && ($isKit || $projectSalesHelper->canSell($product));
+                } elseif (in_array($attributeSetName, explode(',', $apparelItemGroups))) {
+                    $canSell = $projectSalesHelper->canSell($product);
+                } else {
+                    $canSell = $product->isSaleable();
                 }
+
                 $result = !$canSell;
             }
         } catch (\Exception $e) {
