12d6d9c45ed65ddac2a989014716f90555a40cb4
Ning W <phpdev2012@126.com>
2021-10-07 21:43:39 +0800

    Add catalog pricing rules to the GetProduct Endpoint (#1421)
    * Add catalog pricing rules to the GetProduct Endpoint
    
    * Trigger Build

---
+++ Api/Data/ProductInterface.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2017-2021 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Api\Data;
+
+/**
+ * Custom Product interface.
+ *
+ * @api
+ */
+interface ProductInterface extends \Magento\Catalog\Api\Data\ProductInterface
+{
+    /**
+     * Get product final price
+     *
+     * @return float|null
+     */
+    public function getFinalPrice();
+
+    /**
+     * Set product final price
+     *
+     * @param float $price
+     * @return $this
+     */
+    public function setFinalPrice($price);
+}
