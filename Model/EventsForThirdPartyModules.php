42c2b1331158344713e793f1944a8bba79aa007d
John McCombs <john.mccombs@bolt.com>
2021-10-12 23:58:56 +0700

    Add Amasty Prmo Support

--- Model/EventsForThirdPartyModules.php
+++ Model/EventsForThirdPartyModules.php
@@ -48,6 +48,7 @@ use Bolt\Boltpay\ThirdPartyModules\Route\Route as Route_Route;
 use Bolt\Boltpay\Helper\Bugsnag;
 use Bolt\Boltpay\ThirdPartyModules\ImaginationMedia\TmwGiftCard as ImaginationMedia_TmwGiftCard;
 use Bolt\Boltpay\ThirdPartyModules\Amasty\Preorder as Amasty_Preorder;
+use Bolt\Boltpay\ThirdPartyModules\Amasty\Promo as Amasty_Promo;
 use Bolt\Boltpay\ThirdPartyModules\MageWorx\RewardPoints as MageWorx_RewardPoints;
 use Exception;
 
@@ -445,6 +446,15 @@ class EventsForThirdPartyModules
                 ],
             ],
         ],
+        "filterGetBoltCollectSaleRuleDiscounts" => [
+            "listeners" => [
+                [
+                    "module" => "Amasty_Promo",
+                    "checkClasses" => ["Amasty\Promo\Api\Data\GiftRuleInterface"],
+                    "boltClass" => Amasty_Promo::class,
+                ],
+            ],
+        ],
         "collectDiscounts" => [
             "listeners" => [
                 [
@@ -459,6 +469,11 @@ class EventsForThirdPartyModules
                         "Aheadworks\RewardPoints\Model\Config"
                     ],
                     "boltClass" => Aheadworks_RewardPoints::class,
+                ],
+                [
+                    "module" => "Amasty_Promo",
+                    "checkClasses" => ["Amasty\Promo\Api\Data\GiftRuleInterface"],
+                    "boltClass" => Amasty_Promo::class,
                 ],
                 [
                     "module" => "Mirasvit_Rewards",
---
+++ Plugin/Amasty/Promo/Quote/Model/Quote/TotalsCollectorPlugin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Amasty Team
+ * @copyright Copyright (c) 2020 Amasty (https://www.amasty.com)
+ * @package Amasty_Promo
+ */
+
+namespace Bolt\Boltpay\Plugin\Amasty\Promo\Quote\Model\Quote;
+
+use Bolt\Boltpay\Helper\Hook;
+use Bolt\Boltpay\Test\Unit\TestHelper;
+use Amasty\Promo\Plugin\Quote\Model\Quote\TotalsCollectorPlugin as AmastyTotalsCollectorPlugin;
+
+
+class TotalsCollectorPlugin
+{
+    /**
+     * @param AmastyTotalsCollectorPlugin $subject
+     * @param \Magento\Quote\Model\Quote\TotalsCollector $nextSubject
+     * @param callable $nextProceed
+     * @param \Magento\Quote\Model\Quote $quote
+     * @param \Magento\Quote\Model\Quote\Address $address
+     * @throws \ReflectionException
+     */
+    public function beforeAroundCollectAddressTotals(
+        AmastyTotalsCollectorPlugin $subject,
+        \Magento\Quote\Model\Quote\TotalsCollector $nextSubject,
+        callable $nextProceed,
+        \Magento\Quote\Model\Quote $quote,
+        \Magento\Quote\Model\Quote\Address $address
+    )
+    {
+        if (Hook::$fromBolt) {
+            TestHelper::setInaccessibleProperty($subject, 'proceedFlag', false);
+        }
+    }
+}
