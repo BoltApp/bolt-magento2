2179394d623aab5a5ec2091a8fe4dad01ee43a57
Vitaliy Reznikov <vitaliy@bolt.com>
2021-10-18 21:33:21 +0300

    Revert "Add Amasty Prmo Support (#1426)"
    This reverts commit 7a878d23a0cb7cc22495a95db1b89bd74acf2ec4.

--- Model/EventsForThirdPartyModules.php
+++ Model/EventsForThirdPartyModules.php
@@ -48,7 +48,6 @@ use Bolt\Boltpay\ThirdPartyModules\Route\Route as Route_Route;
 use Bolt\Boltpay\Helper\Bugsnag;
 use Bolt\Boltpay\ThirdPartyModules\ImaginationMedia\TmwGiftCard as ImaginationMedia_TmwGiftCard;
 use Bolt\Boltpay\ThirdPartyModules\Amasty\Preorder as Amasty_Preorder;
-use Bolt\Boltpay\ThirdPartyModules\Amasty\Promo as Amasty_Promo;
 use Bolt\Boltpay\ThirdPartyModules\MageWorx\RewardPoints as MageWorx_RewardPoints;
 use Bolt\Boltpay\ThirdPartyModules\MageWorx\ShippingRules as MageWorx_ShippingRules;
 use Exception;
@@ -456,15 +455,6 @@ class EventsForThirdPartyModules
                 ],
             ],
         ],
-        "filterGetBoltCollectSaleRuleDiscounts" => [
-            "listeners" => [
-                [
-                    "module" => "Amasty_Promo",
-                    "checkClasses" => ["Amasty\Promo\Api\Data\GiftRuleInterface"],
-                    "boltClass" => Amasty_Promo::class,
-                ],
-            ],
-        ],
         "collectDiscounts" => [
             "listeners" => [
                 [
@@ -479,11 +469,6 @@ class EventsForThirdPartyModules
                         "Aheadworks\RewardPoints\Model\Config"
                     ],
                     "boltClass" => Aheadworks_RewardPoints::class,
-                ],
-                [
-                    "module" => "Amasty_Promo",
-                    "checkClasses" => ["Amasty\Promo\Api\Data\GiftRuleInterface"],
-                    "boltClass" => Amasty_Promo::class,
                 ],
                 [
                     "module" => "Mirasvit_Rewards",
