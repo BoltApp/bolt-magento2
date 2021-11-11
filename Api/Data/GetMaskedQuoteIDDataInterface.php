9fd059723550952648da99d2601c75491c776554
Vitaliy Reznikov <vitaliy@bolt.com>
2021-11-11 10:56:54 +0000

    Implement get masked it endpoint

---
+++ Api/Data/GetMaskedQuoteIDDataInterface.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2017-2021 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Bolt\Boltpay\Api\Data;
+
+/**
+ * Get Masked Quote Id interface.
+ *
+ * @api
+ */
+interface GetMaskedQuoteIDDataInterface
+{
+
+    /**
+     * Get masked quote id.
+     *
+     * @api
+     * @return string
+     */
+    public function getMaskedQuoteID();
+
+    /**
+     * Set product info.
+     *
+     * @api
+     * @param string
+     * @return $this
+     */
+    public function setMaskedQuoteID($id);
+}
