def5ab538035c2888d8c469a7b1da8641617909b
Yaroslav Garmash <garmashdev@gmail.com>
2022-02-21 16:56:31 +0300

    API order transactions save endpoint

---
+++ Api/OrderTransactionsInterface.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2017-2022 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Api;
+
+use Magento\Sales\Api\Data\TransactionInterface;
+
+/**
+ * Order transactions interface. Transactions save endpoint.
+ *
+ * Save order transactions
+ * @api
+ */
+interface OrderTransactionsInterface
+{
+    /**
+     * Save order transaction
+     *
+     * @param TransactionInterface $transaction
+     * @param string[] $additionalInformation
+     * @return int
+     */
+    public function execute(
+        TransactionInterface $transaction,
+        array $additionalInformation
+    ): int;
+}
