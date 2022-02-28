789d49e59495aeaa3a24260dbefe1f31cd41456f
Yaroslav Garmash <garmashdev@gmail.com>
2022-02-28 17:42:32 +0300

    API order transactions save endpoint - changed test to integration

--- Test/Unit/Model/Api/OrderTransactionsTest.php
+++ Test/Unit/Model/Api/OrderTransactionsTest.php
@@ -14,14 +14,14 @@
  * @copyright  Copyright (c) 2017-2022 Bolt Financial, Inc (https://www.bolt.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 namespace Bolt\Boltpay\Test\Unit\Model\Api;
 
-use Magento\TestFramework\TestCase\WebapiAbstract;
-use Magento\Framework\Webapi\Rest\Request as RestRequest;
+use Bolt\Boltpay\Test\Unit\BoltTestCase;
+use Bolt\Boltpay\Test\Unit\TestUtils;
 use Magento\TestFramework\Helper\Bootstrap;
-use Magento\Framework\ObjectManagerInterface;
-use Magento\Sales\Model\Order;
+use Magento\TestFramework\ObjectManager;
+use Bolt\Boltpay\Model\Api\OrderTransactions;
+use Magento\Sales\Model\Order\Payment\Transaction;
 use Magento\Sales\Model\Order\Payment\Transaction\Repository;
 
 /**
@@ -29,67 +29,56 @@ use Magento\Sales\Model\Order\Payment\Transaction\Repository;
  * @package Bolt\Boltpay\Test\Unit\Model\Api
  * @coversDefaultClass \Bolt\Boltpay\Model\Api\OrderTransactions
  */
-class OrderTransactionsTest extends WebapiAbstract
+class OrderTransactionsTest extends BoltTestCase
 {
-    const RESOURCE_PATH = '/V1/bolt/boltpay/transactions';
-
-    const SERVICE_READ_NAME = 'boltpayOrderTransactionsV1';
-
-    const SERVICE_VERSION = 'V1';
-
     const TXN_ID = 'test_txt_id';
 
     /**
-     * @var ObjectManagerInterface
+     * @var ObjectManager
      */
     protected $objectManager;
 
     /**
-     * @return void
+     * @var OrderTransactions
      */
-    protected function setUp(): void
-    {
-        $this->objectManager = Bootstrap::getObjectManager();
-    }
+    private $orderTransactions;
 
     /**
-     * @magentoApiDataFixture Magento/Sales/_files/order.php
+     * @inheritdoc
+     */
+    protected function setUpInternal()
+    {
+        if (!class_exists('\Magento\TestFramework\Helper\Bootstrap')) {
+            return;
+        }
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->orderTransactions = $this->objectManager->create(OrderTransactions::class);
+    }
+
+
+    /**
+     * @test
      */
     public function testOrderTransactionSuccessCreation()
     {
-        $order = $this->objectManager->create(Order::class)
-            ->loadByIncrementId('100000001');
-
-        $serviceInfo = [
-            'rest' => [
-                'resourcePath' => self::RESOURCE_PATH,
-                'httpMethod' => RestRequest::HTTP_METHOD_POST,
-            ],
-            'soap' => [
-                'service' => self::SERVICE_READ_NAME,
-                'serviceVersion' => self::SERVICE_VERSION,
-                'operation' => self::SERVICE_READ_NAME . 'execute',
-            ],
-        ];
-
-        $requestData = [
-            'transaction' => [
+        $order = TestUtils::createDumpyOrder();
+        $transactionObj = $this->objectManager->create(Transaction::class,);
+        $transactionObj->setData(
+            [
                 'order_id' => $order->getId(),
                 'payment_id' => $order->getPayment()->getId(),
                 'txn_id' => self::TXN_ID,
                 'parent_txn_id' => 'TA9EsNAYR6Zrf-auth',
                 'txn_type' => 'capture',
                 'is_closed' => 1
-            ],
-            'additional_information' => [
+            ]
+        );
+        $transactionId = $this->orderTransactions->execute(
+            $transactionObj,
+            [
                 'testKey' => 'testData'
             ]
-        ];
-
-        $transactionId = $this->_webApiCall($serviceInfo, $requestData);
-        /**
-         * @var Repository $orderTransactionRepository
-         */
+        );
         $orderTransactionRepository = $this->objectManager->get(Repository::class);
         $transaction = $orderTransactionRepository->get($transactionId);
         $this->assertEquals(self::TXN_ID, $transaction->getTxnId());
