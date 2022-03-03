c5f0a20f15f5c88562cfea6b4fb5cf62a79eb114
Yaroslav Garmash <garmashdev@gmail.com>
2022-03-03 18:41:37 +0300

    Sales order grid filter by bolt payments method credit card types fix

---
+++ Plugin/Magento/Framework/View/Element/UiComponent/DataProvider/RegularFilterPlugin.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2017-2022 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Bolt\Boltpay\Plugin\Magento\Framework\View\Element\UiComponent\DataProvider;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\RegularFilter;
+use Magento\Framework\Data\Collection;
+use Magento\Framework\Api\Filter;
+use Magento\Framework\App\ResourceConnection;
+use \Magento\Sales\Model\ResourceModel\Order\Grid\Collection as OrderGridCollection;
+use Bolt\Boltpay\Helper\Config;
+use Bolt\Boltpay\Model\Payment;
+
+/**
+ * Ui component filter plugin to implement filter sales order grid by bolt payment methods
+ */
+class RegularFilterPlugin
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @param Config $config
+     * @param ResourceConnection $resourceConnection
+     */
+    public function __construct(
+        Config $config,
+        ResourceConnection $resourceConnection
+    ) {
+        $this->config = $config;
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    /**
+     * Sales order grid filtering by bolt credit card type
+     *
+     * @param RegularFilter $subject
+     * @param callable $proceed
+     * @param Collection $collection
+     * @param Filter $filter
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function aroundApply(
+        RegularFilter $subject,
+        callable $proceed,
+        Collection $collection,
+        Filter $filter
+    ) {
+        if ($this->config->getShowCcTypeInOrderGrid() &&
+            $collection instanceof OrderGridCollection &&
+            $filter->getField() == 'payment_method' &&
+            strpos($filter->getValue(), Payment::METHOD_CODE . '_') !== false
+        ) {
+            $collection->getSelect()->joinLeft(
+                ['payment' => $this->resourceConnection->getTableName('sales_order_payment')],
+                'main_table.entity_id = payment.parent_id',
+                ['cc_type' => 'LOWER(cc_type)']
+            );
+
+            $collection->getSelect()
+                ->where(
+                    'main_table.payment_method = "'. Payment::METHOD_CODE .'" AND cc_type = ?',
+                    str_replace(Payment::METHOD_CODE . '_', '', $filter->getValue())
+                );
+
+        } else {
+            return $proceed($collection, $filter);
+        }
+    }
+}
