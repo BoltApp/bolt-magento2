3e66e3ded4593ebd66c735b641c047ca88c58a37
Yaroslav Garmash <ygarmash@bolt.com>
2022-06-15 18:49:30 +0300

    catalog ingestion: integration tests

--- Test/Unit/Model/CatalogIngestion/ProductEventRequestBuilderTest.php
+++ Test/Unit/Model/CatalogIngestion/ProductEventRequestBuilderTest.php
@@ -18,11 +18,16 @@
 namespace Bolt\Boltpay\Test\Unit\Model\CatalogIngestion;
 
 use Bolt\Boltpay\Api\ProductEventRepositoryInterface;
+use Bolt\Boltpay\Helper\Config as BoltConfig;
+use Bolt\Boltpay\Helper\FeatureSwitch\Decider;
+use Bolt\Boltpay\Model\CatalogIngestion\ProductEventProcessor;
 use Bolt\Boltpay\Test\Unit\BoltTestCase;
+use Bolt\Boltpay\Test\Unit\TestHelper;
 use Bolt\Boltpay\Test\Unit\TestUtils;
 use Bolt\Boltpay\Model\CatalogIngestion\ProductEventRequestBuilder;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Store\Model\ScopeInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Store\Model\StoreManagerInterface;
@@ -78,6 +83,20 @@ class ProductEventRequestBuilderTest extends BoltTestCase
         $this->resource = $this->objectManager->get(ResourceConnection::class);
         $this->storeManager = $this->objectManager->get(StoreManagerInterface::class);
         $this->productEventRepository = $this->objectManager->get(ProductEventRepositoryInterface::class);
+        $productEventProcessor = $this->objectManager->get(ProductEventProcessor::class);
+        $featureSwitches = $this->createMock(Decider::class);
+        $featureSwitches->method('isCatalogIngestionEnabled')->willReturn(true);
+        TestHelper::setProperty($productEventProcessor, 'featureSwitches', $featureSwitches);
+        $websiteId = $this->storeManager->getWebsite()->getId();
+        $configData = [
+            [
+                'path' => BoltConfig::XML_PATH_CATALOG_INGESTION_ENABLED,
+                'value' => 1,
+                'scope' => ScopeInterface::SCOPE_WEBSITES,
+                'scopeId' => $websiteId,
+            ]
+        ];
+        TestUtils::setupBoltConfig($configData);
     }
 
     /**
