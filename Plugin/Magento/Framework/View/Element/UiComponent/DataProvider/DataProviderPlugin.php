3766581bfb2a09c2a8404c188bd1ae77b4e38fbd
Ning W <phpdev2012@126.com>
2022-07-17 08:25:15 +0800

    Fix runtime-error with PHP8.1 (#1614)
    * Fix runtime-error with PHP8.1
    
    * update

--- Plugin/Magento/Framework/View/Element/UiComponent/DataProvider/DataProviderPlugin.php
+++ Plugin/Magento/Framework/View/Element/UiComponent/DataProvider/DataProviderPlugin.php
@@ -95,9 +95,9 @@ class DataProviderPlugin
             if (!$payment) {
                 continue;
             }
-            if ($showCcTypeInOrderGrid &&
+            if ($showCcTypeInOrderGrid && ! empty($ccType = $payment->getCcType()) &&
                 key_exists(
-                    $ccType = strtolower($payment->getCcType()),
+                    $ccType = strtolower($ccType),
                     Order::SUPPORTED_CC_TYPES
                 )
             ) {
