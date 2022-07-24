9d0c174ca554b2dcb00eca97a115ab150151a096
Ning W <phpdev2012@126.com>
2022-07-24 16:48:24 +0800

    updates

--- Test/scripts/phpcs_meqp2.sh
+++ Test/scripts/phpcs_meqp2.sh
@@ -10,9 +10,8 @@ composer show -i
 echo "{\"http-basic\":{\"repo.magento.com\":{\"username\":\"${MAGENTO_PUBLIC_KEY}\",\"password\":\"${MAGENTO_PRIVATE_KEY}\"}}}" > $HOME/.composer/auth.json
 
 cd ..
-composer create-project --repository=https://repo.magento.com magento/marketplace-eqp
-composer create-project magento/magento-coding-standard --stability=dev magento-coding-standard -n
+composer create-project magento/magento-coding-standard --stability=dev magento-coding-standard
 
 cd magento-coding-standard
 
-vendor/bin/phpcs ../project --standard=../project/phpcs.xml --colors --severity=10 --extensions=php,phtml
+vendor/bin/phpcs ../project --standard=Magento2 --colors --severity=10
