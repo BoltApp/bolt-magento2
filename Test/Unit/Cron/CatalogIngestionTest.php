afd65c47b5cc1069cf304ae5ce8d99b1dc1aeb34
Yaroslav Garmash <ygarmash@bolt.com>
2022-07-28 18:29:37 +0300

    catalog ingestion: added api keys validation

--- Test/Unit/Cron/CatalogIngestionTest.php
+++ Test/Unit/Cron/CatalogIngestionTest.php
@@ -15,7 +15,7 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-namespace Bolt\Boltpay\Test\Unit\Helper;
+namespace Bolt\Boltpay\Test\Unit\Cron;
 
 use Bolt\Boltpay\Api\Data\ProductEventInterface;
 use Bolt\Boltpay\Api\ProductEventRepositoryInterface;
