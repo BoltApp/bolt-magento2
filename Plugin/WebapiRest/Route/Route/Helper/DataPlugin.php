4266798400b9de0ffefa3ca6b889e1b15588d300
Andrii Kylchytskyi <andrii.kylchytskyi@bolt.com>
2022-09-30 13:57:01 +0300

    Added plugin to override Route_Route quote getter function to get quote from db

--- Plugin/WebapiRest/Route/Route/Helper/DataPlugin.php
+++ Plugin/WebapiRest/Route/Route/Helper/DataPlugin.php
@@ -22,15 +22,31 @@ namespace Bolt\Boltpay\Plugin\WebapiRest\Route\Route\Helper;
  */
 class DataPlugin
 {
-    public const IS_INSURED_PARAM = 'routeIsInsured';
+    public const CART_ID_PARAM = 'cartId';
+    public const IS_INSURED_PARAM = 'route_is_insured';
     /**
      * @var \Magento\Framework\Webapi\Rest\Request
      */
     private $request;
 
-    public function __construct(\Magento\Framework\Webapi\Rest\Request $request)
-    {
+    /**
+     * @var \Magento\Quote\Api\CartRepositoryInterface
+     */
+    private $quoteRepository;
+
+    /**
+     * @var \Bolt\Boltpay\Model\ThirdPartyModuleFactory
+     */
+    private $boltRouteQuoteClient;
+
+    public function __construct(
+        \Magento\Framework\Webapi\Rest\Request $request,
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
+        \Bolt\Boltpay\Model\ThirdPartyModuleFactory $boltRouteQuoteClient
+    ){
         $this->request = $request;
+        $this->quoteRepository = $quoteRepository;
+        $this->boltRouteQuoteClient = $boltRouteQuoteClient;
     }
 
     public function afterIsInsured(\Route\Route\Helper\Data $subject, $result): bool
@@ -38,6 +54,35 @@ class DataPlugin
         $requestParams = $this->request->getParams();
         if (array_key_exists(self::IS_INSURED_PARAM, $requestParams)) {
             return filter_var($requestParams[self::IS_INSURED_PARAM], FILTER_VALIDATE_BOOLEAN);
+        }
+        return $result;
+    }
+
+    public function afterGetQuoteResponse(\Route\Route\Helper\Data $subject, $result): bool
+    {
+        $requestParams = $this->request->getParams();
+        if (array_key_exists(self::IS_INSURED_PARAM, $requestParams)) {
+            $cartId = $requestParams[self::CART_ID_PARAM];
+            $quote = $this->quoteRepository->get($cartId);
+
+            if (!$quote) {
+                return false;
+            }
+
+            $subtotal = $quote->getSubtotal() ? $quote->getSubtotal() : 0;
+            $amountCovered = $this->getShippableItemsSubtotal($quote);
+            if (!$subtotal || $subtotal==0 || !$amountCovered || $amountCovered==0) {
+                return true;
+            }
+
+            $quoteClient = $this->boltRouteQuoteClient->getInstance();
+
+            return $quoteClient->getQuote(
+                $subtotal,
+                $amountCovered,
+                filter_var($requestParams[self::IS_INSURED_PARAM], FILTER_VALIDATE_BOOLEAN),
+                $quote->getQuoteCurrencyCode()
+            );
         }
         return $result;
     }
