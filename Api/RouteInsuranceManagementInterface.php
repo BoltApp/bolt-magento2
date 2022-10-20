538dc87ca31a9b707873e96ad06156e2c137ea1b
Andrii Kylchytskyi <48790390+KyerdnA@users.noreply.github.com>
2022-10-20 12:41:24 +0300

    Created custom API endpoiont to enable or disable Route (#1653)
    * Created custom API endpoiont to enable or disable Route
    
    * Added plugin to override Route_Route quote getter function to get quote from db
    
    * Added tests & grant total into success response body
    
    * fix type argument declaration
    
    * Optimize code
    
    * Replace plugin feature implementation by session implementation
    
    * Fix tests
    
    * Added new constructor argument to mock object
    
    * Optimize tests
    
    * Optimize tests
    
    * disable original contructor for mock object
    
    * replace mock for private function by mock on object
    
    * Rebuild mock
    
    * tests optimization
    
    * validation fix
    
    Co-authored-by: Andrii Kylchytskyi <andrii.kylchytskyi@bolt.com>
    Co-authored-by: Yaroslav Garmash <ygarmash@bolt.com>

---
+++ Api/RouteInsuranceManagementInterface.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2017-2022 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bolt\Boltpay\Api;
+
+use Magento\Framework\Webapi\Exception as WebapiException;
+
+/**
+ * Route insurance interface. Route endpoint.
+ *
+ * Enable or disable route insurance.
+ * @api
+ */
+interface RouteInsuranceManagementInterface
+{
+    const ROUTE_MODULE_NAME = 'Route_Route';
+    const RESPONSE_SUCCESS_STATUS = 200;
+    const RESPONSE_FAIL_STATUS = 404;
+    public const CART_ID_PARAM = 'cartId';
+
+    /**
+     * Enable or disable route insurance.
+     *
+     * @api
+     * @param string $cartId quote id
+     * @param bool $routeIsInsured route enabled status
+     * @return void
+     *
+     * @throws WebapiException
+     */
+    public function execute($cartId, $routeIsInsured);
+}
