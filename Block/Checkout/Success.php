a3d97fede7a0ee5cc28d21ce37e7127e69623263
Elie Lloyd <elie@lloydworks.com>
2022-11-02 16:43:18 +0000

    Bugfix: Notice Undefined property on the checkout success page

--- Block/Checkout/Success.php
+++ Block/Checkout/Success.php
@@ -21,6 +21,8 @@ namespace Bolt\Boltpay\Block\Checkout;
 use Bolt\Boltpay\Block\BlockTrait;
 use Bolt\Boltpay\Helper\Config;
 use Bolt\Boltpay\Helper\FeatureSwitch\Decider;
+use Bolt\Boltpay\Model\EventsForThirdPartyModules;
+use Magento\Framework\Session\SessionManager as CheckoutSession;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 
@@ -29,21 +31,37 @@ class Success extends Template
     use BlockTrait;
 
     /**
+     * @var CheckoutSession
+     */
+    private $checkoutSession;
+
+    /**
+     * @var EventsForThirdPartyModules
+     */
+    protected $eventsForThirdPartyModules;
+
+    /**
      * Success constructor.
      *
      * @param Config  $configHelper
      * @param Context $context
      * @param Decider $featureSwitches
+     * @param CheckoutSession $checkoutSession
+     * @param EventsForThirdPartyModules $eventsForThirdPartyModules
      * @param array   $data
      */
     public function __construct(
         Config $configHelper,
         Context $context,
         Decider $featureSwitches,
+        CheckoutSession $checkoutSession,
+        EventsForThirdPartyModules $eventsForThirdPartyModules,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->configHelper = $configHelper;
         $this->featureSwitches = $featureSwitches;
+        $this->checkoutSession = $checkoutSession;
+        $this->eventsForThirdPartyModules = $eventsForThirdPartyModules;
     }
 }
