e0bc3faba3673185b9b501ca428f46816954bde5
Vitaliy Reznikov <vitaliy@bolt.com>
2022-11-21 10:32:59 +0200

    Bump version to 2.26.3 (#1690)

--- Test/Unit/Block/JsTest.php
+++ Test/Unit/Block/JsTest.php
@@ -1398,7 +1398,7 @@ class JsTest extends BoltTestCase
      */
     public function getModuleVersion_always_returnsModuleVersion()
     {
-        $moduleVersion = '2.26.2';
+        $moduleVersion = '2.26.3';
         static::assertEquals($moduleVersion, $this->block->getModuleVersion());
     }
 
