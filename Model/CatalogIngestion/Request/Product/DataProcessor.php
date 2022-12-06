de2c9199d3a4bd6e5c2bfed1ff05f3a88d6c3cdf
Yaroslav Garmash <ygarmash@bolt.com>
2022-12-06 17:17:23 +0300

    Catalog ingestion: fix stock status for disabled Magento_InventoryCatalog m2 istallation

--- Model/CatalogIngestion/Request/Product/DataProcessor.php
+++ Model/CatalogIngestion/Request/Product/DataProcessor.php
@@ -792,7 +792,9 @@ class DataProcessor
      */
     private function getProductAvailability(ProductInterface $product): string
     {
-        $isAvailable = $product->isAvailable();
+        $isAvailable = ($this->moduleManager->isEnabled('Magento_InventoryCatalog')) ?
+            $product->isAvailable() : $product->getExtensionAttributes()->getStockItem()->getIsInStock();
+
         if ($product->getTypeId() == Configurable::TYPE_CODE) {
             $stockItem = $product->getExtensionAttributes()->getStockItem();
             $isAvailable = (bool)$stockItem->getIsInStock() && $product->getQuantityAndStockStatus()['is_in_stock'];
