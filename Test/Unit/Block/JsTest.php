839cefc4200f1226093b3cabfec461d44f77d8b0
Alex Portillo <82459455+alexportillo-bolt@users.noreply.github.com>
2023-01-30 13:29:24 -0500

    Bump version to 2.26.5 (#1746)

--- Test/Unit/Block/JsTest.php
+++ Test/Unit/Block/JsTest.php
@@ -1398,7 +1398,7 @@ class JsTest extends BoltTestCase
      */
     public function getModuleVersion_always_returnsModuleVersion()
     {
-        $moduleVersion = '2.26.4';
+        $moduleVersion = '2.26.5';
         static::assertEquals($moduleVersion, $this->block->getModuleVersion());
     }
 
