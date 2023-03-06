183731a990ebe691fab4215c2b5a89ba7a5546b7
Vitaliy Reznikov <vitaliy@bolt.com>
2023-03-06 15:56:23 +0200

    Increase phpcs severity from 10 to 9

--- Test/scripts/phpcs_meqp2.sh
+++ Test/scripts/phpcs_meqp2.sh
@@ -14,4 +14,4 @@ composer create-project magento/magento-coding-standard --stability=dev magento-
 
 cd magento-coding-standard
 
-vendor/bin/phpcs ../project --standard=Magento2 --colors --severity=10
+vendor/bin/phpcs ../project --standard=Magento2 --colors --severity=9
