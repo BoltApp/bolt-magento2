2b94a12f2a585965136c194507a3e99c51c5d161
Vitaliy Reznikov <vitaliy@bolt.com>
2023-03-06 12:15:20 +0200

    up

--- Test/scripts/phpcs_meqp2.sh
+++ Test/scripts/phpcs_meqp2.sh
@@ -4,11 +4,7 @@ set -e
 set -u
 set -x
 
-#sudo composer self-update -q
 composer show -i
-pwd
-
-#echo "{\"http-basic\":{\"repo.magento.com\":{\"username\":\"${MAGENTO_PUBLIC_KEY}\",\"password\":\"${MAGENTO_PRIVATE_KEY}\"}}}" > $HOME/.composer/auth.json
 
 cd ..
 
@@ -18,29 +14,24 @@ cd magento-coding-standard
 
 vendor/bin/phpcs ../project --standard=Magento2 --colors --severity=10 -p
 
-pwd
-
 cd ../project
 export MAGENTO_VERSION="2.3.0"
 
 Test/scripts/install_magento.sh
-pwd
 cd ..
-pwd
+
+
 mkdir -p magento/app/code/Bolt/Boltpay
 mv project/.circleci/phpstan/* magento/app/code/Bolt/Boltpay/
 mv project/* magento/app/code/Bolt/Boltpay/
 cd magento
-pwd
+
 # generate classes for phpstan
 php -dmemory_limit=5G bin/magento module:enable Bolt_Boltpay
 php -dmemory_limit=5G bin/magento setup:di:compile
 cd ..
-pwd
 cd magento/app/code/Bolt/Boltpay
-pwd
 wget https://github.com/phpstan/phpstan/releases/download/1.10.3/phpstan.phar
 chmod +x phpstan.phar
-#cp /home/circleci/project/.circleci/phpstan.neon phpstan.neon
-ls
-php -dmemory_limit=5G ./phpstan.phar analyse --level=0 --debug
+
+php -dmemory_limit=5G ./phpstan.phar analyse --level=0 --xdebug
