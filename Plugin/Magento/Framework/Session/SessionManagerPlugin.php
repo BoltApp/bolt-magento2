175456ab84b8de793fe9b1334433081d02bd1ea3
Yaroslav Garmash <ygarmash@bolt.com>
2023-09-26 13:47:26 +0300

    php cs fix

--- Plugin/Magento/Framework/Session/SessionManagerPlugin.php
+++ Plugin/Magento/Framework/Session/SessionManagerPlugin.php
@@ -256,7 +256,7 @@ class SessionManagerPlugin
         if (!class_exists($boltSessionParam[self::BOLT_SESSION_PARAM_SESSION_CLASS_NAME_KEY])) {
             $msg = sprintf(
                 'The provided [%s] class is not exist',
-                $boltSessionParam[self::BOLT_SESSION_PARAM_SESSION_CLASS_NAME_KEY],
+                $boltSessionParam[self::BOLT_SESSION_PARAM_SESSION_CLASS_NAME_KEY]
             );
             throw new \InvalidArgumentException($msg);
         }
