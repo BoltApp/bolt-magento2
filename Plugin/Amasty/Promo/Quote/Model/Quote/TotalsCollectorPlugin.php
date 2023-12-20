369d1638b98df79a88e8f7f262892ab65b9ca18e
Yaroslav Garmash <ygarmash@bolt.com>
2023-12-20 17:26:29 +0300

    Legacy amasty promo items mismatch fix (#1876)
    * Legacy amasty promo items mismatch fix
    
    * fix php:cs
    
    * added comment

--- Plugin/Amasty/Promo/Quote/Model/Quote/TotalsCollectorPlugin.php
+++ Plugin/Amasty/Promo/Quote/Model/Quote/TotalsCollectorPlugin.php
@@ -28,6 +28,14 @@ class TotalsCollectorPlugin
         \Magento\Quote\Model\Quote\Address $address
     )
     {
+        // legacy flow: skip amasty promo calculation for immutable quote collect total calls
+        // amasty promo define the db table with relations between quote id and promo items
+        // because id's of immutable quote and original quote are different, amasty promo can't find promo items status during immutable quote collect totals
+        // which leads to mismatch between original quote and immutable quote which leads to restoring already removed items in card
+        if ($quote->getBoltParentQuoteId() != null && $quote->getBoltParentQuoteId() != $quote->getId()) {
+            $address->setData('amastyFreeGiftProcessed', true);
+        }
+
         if (Hook::$is_from_remove_discount_hook) {
             $proceedFlag = function () use ($subject) {
                 $subject->proceedFlag = false;
