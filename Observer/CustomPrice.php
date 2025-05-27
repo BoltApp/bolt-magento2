a60a531aad61fb559b8567f83156193622d17b75
bolt-raghuraaj <rraaj@bolt.com>
2025-05-27 11:09:14 +0530

    Add validation for Bolt API requests in CustomPrice observer
    Introduced a RestApiRequestValidator to validate incoming Bolt API requests in the CustomPrice observer. This ensures only legitimate requests are processed, improving security and request handling reliability.

--- Observer/CustomPrice.php
+++ Observer/CustomPrice.php
@@ -1,3 +1,21 @@
+<?php
+/**
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ *
+ * @copyright  Copyright (c) 2017-2023 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 namespace Bolt\Boltpay\Observer;
 
 use Magento\Framework\Event\Observer;
@@ -66,7 +84,7 @@ class CustomPrice implements ObserverInterface
             }
             $subtotal += $item->getRowTotal();
         }
-
+        // If client sent a price, use that; otherwise 1% fee
         if ($boltItem && $subtotal > 0) {
             $customPrice = ($sentFee > 0) ? $sentFee : $this->priceCurrency->round($subtotal * 0.01);
             $boltItem->setCustomPrice($customPrice);
